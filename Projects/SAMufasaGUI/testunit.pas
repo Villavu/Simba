ea1cac880f1cdc85aab7bd441cdbb6384c4a8e8c
Wizzup? <Wizzup?@3f818213-9676-44b0-a9b4-5e4c4e03d09d>
2009-09-04 06:06:58 +0000

    Added a test. Works nicely.
    git-svn-id: http://www.villavu.com/repositories/merlijn/mufasa@25 3f818213-9676-44b0-a9b4-5e4c4e03d09d

--- Projects/SAMufasaGUI/testunit.pas
+++ Projects/SAMufasaGUI/testunit.pas
@@ -42,6 +42,18 @@ type
   end;
 
 procedure TMyThread.Execute;
+Const
+    TestPath = './test';
+
+    fsFromBeginning = 0; // offset must be pos or 0
+    fsFromCurrent = 1; // offset pos or neg
+    fsFromEnd = 2; // offset only neg or 0
+
+    // put somewhese else
+    DirectorySeperator = '/';
+    DS = '/';
+    ExeExt = '';
+
 Var
    Client: TClient;
    w,h, x, y, xx, yy, i:integer;
@@ -49,6 +61,10 @@ Var
    ReturnData : TRetData;
    arr: Array Of Integer;
    LoopY,LoopX : integer;
+
+  s, s2: string;
+  myFile, myFile2: Integer;
+
 
 begin
   while (not Terminated)  do
@@ -137,6 +153,67 @@ begin
     writeln('Right mouse is down!');
   if Client.MInput.IsMouseButtonDown(mouse_Middle) then
     writeln('Middle mouse is down!');
+
+
+  with Client.MFiles do
+  begin
+  s := ExtractFileDir(Application.ExeName);
+  Writeln('Our current path is: ' + s);
+  If DirectoryExists(s) Then
+    writeln('Directory ' + s + ' exists.');
+
+  If FileExists(s + DirectorySeperator + 'Cogat' + ExeExt) Then
+    writeln('We exist!');
+
+  myFile := CreateFile(TestPath);
+  WriteFileString(myFile, 'wat');
+  WriteFileString(myFile, 'watnumber2');
+  CloseFile(myFile);
+
+  myFile := OpenFile(TestPath, False);
+  ReadFileString(myFile, s2, 2);
+  writeln('s2: ' + s2);
+  CloseFile(myFile);
+
+  myFile := CreateFile(TestPath + '2');
+  WriteFileString(myFile, 'wat222');
+
+  CloseFile(myFile);
+
+  // TestPath now contains; 'watwatnumber2'. We will make it write 'number',
+  // and then 2.
+  myFile := OpenFile(TestPath, False);
+  SetFileCharPointer(myFile, 6, fsFromBeginning);
+  ReadFileString(myFile, s2, 6);
+  writeln('s2: ' + s2);
+  s2 := '';
+
+  SetFileCharPointer(myFile, -1, fsFromEnd);
+  ReadFileString(myFile, s2, 1);
+  writeln('s2: ' + s2);
+  CloseFile(myFile);
+ // myFile2 should be -1.
+  myFile := RewriteFile(TestPath, False);
+  myFile2 := RewriteFile(TestPath, False);
+  writeln(inttostr(myFile) + ' : ' + inttostr(myFile2));
+
+  // myFile2 should be -1.
+  myFile2 := OpenFile(TestPath, False);
+  writeln(inttostr(myFile) + ' : ' + inttostr(myFile2));
+
+  // Now, we will test EndOfFile, and FileSize.
+
+  While Not EndOfFile(myFile) Do
+  Begin
+    ReadFileString(myFile, s2, 1);
+    Writeln(s2);
+  End;
+
+  CloseFile(myFile);
+  writeln('wat');
+
+
+  end;
   Client.Destroy;
   writeln('Test completed successfully');
   break;
