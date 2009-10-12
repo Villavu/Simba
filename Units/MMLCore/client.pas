18dd345e9456671c784623443c90b2ebd25317c8
Wizzup? <Wizzup?@3f818213-9676-44b0-a9b4-5e4c4e03d09d>
2009-10-12 14:21:37 +0000

    I meant aGPL. Fixed spaces.
    git-svn-id: http://www.villavu.com/repositories/merlijn/mufasa@123 3f818213-9676-44b0-a9b4-5e4c4e03d09d

--- Units/MMLCore/client.pas
+++ Units/MMLCore/client.pas
@@ -6,9 +6,9 @@
 	See the file COPYING, included in this distribution,
 	for details about the copyright.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
 
 }
 
