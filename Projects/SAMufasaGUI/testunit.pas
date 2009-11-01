5d4f147e518126408a0d6c9a0d07d191236d0468
Raymond <Raymond@3f818213-9676-44b0-a9b4-5e4c4e03d09d>
2009-11-01 15:17:18 +0000

    Couldn't switch tabs anymore
    git-svn-id: http://www.villavu.com/repositories/merlijn/mufasa@169 3f818213-9676-44b0-a9b4-5e4c4e03d09d

--- Projects/SAMufasaGUI/testunit.pas
+++ Projects/SAMufasaGUI/testunit.pas
@@ -767,7 +767,7 @@ end;
 procedure TForm1.PageControl1MouseDown(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
 begin
-  PageControl1.BeginDrag(false);
+  PageControl1.BeginDrag({$ifdef linux}true{$else} false {$endif});
 end;
 
 
