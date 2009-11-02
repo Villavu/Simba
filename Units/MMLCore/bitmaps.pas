6936a5c81e1dd887fd1ff7b2fecde8598aaa63f1
Raymond <Raymond@3f818213-9676-44b0-a9b4-5e4c4e03d09d>
2009-11-02 09:25:49 +0000

    Made some changes in CopyClientToBitmap (It does NOT have a bug, unless ReturnData is wrong.. Might wanne look into that Tovenaar) it now has the option to resize the bitmap or not. (Scar does resize the bitmap).
    git-svn-id: http://www.villavu.com/repositories/merlijn/mufasa@174 3f818213-9676-44b0-a9b4-5e4c4e03d09d

--- Units/MMLCore/bitmaps.pas
+++ Units/MMLCore/bitmaps.pas
@@ -59,7 +59,7 @@ type
     procedure FastDrawClear(Color : TColor);
     procedure FastDrawTransparent(x, y: Integer; TargetBitmap: TMufasaBitmap);
     procedure FastReplaceColor(OldColor, NewColor: TColor);
-    procedure CopyClientToBitmap(MWindow : TObject; xs, ys, xe, ye: Integer);
+    procedure CopyClientToBitmap(MWindow : TObject;Resize : boolean; xs, ys, xe, ye: Integer);
     procedure RotateBitmap(angle: Extended;TargetBitmap : TMufasaBitmap );
     procedure Desaturate;overload;
     procedure Desaturate(TargetBitmap : TMufasaBitmap); overload;
@@ -471,12 +471,15 @@ begin
       FData[i] := NewCol;
 end;
 
-procedure TMufasaBitmap.CopyClientToBitmap(MWindow : TObject; xs, ys, xe, ye: Integer);
+procedure TMufasaBitmap.CopyClientToBitmap(MWindow : TObject;Resize : boolean; xs, ys, xe, ye: Integer);
 var
-  wi,hi,y : integer;
+  y : integer;
+  wi,hi : integer;
   PtrRet : TRetData;
   Rows : integer;
 begin
+  if Resize then
+    Self.SetSize(xe-xs+1,ye-ys+1);
   wi := Min(xe-xs + 1,Self.w);
   hi := Min(ye-ys + 1,Self.h);
   PtrRet := TMWindow(MWindow).ReturnData(xs,ys,wi,hi);
