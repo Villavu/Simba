366f33fb0e8c6e24b8ad90fe6a59c78887aaba09
Wizzup? <Wizzup?@3f818213-9676-44b0-a9b4-5e4c4e03d09d>
2009-12-31 20:38:40 +0000

    Few changes
    git-svn-id: http://www.villavu.com/repositories/merlijn/mufasa@354 3f818213-9676-44b0-a9b4-5e4c4e03d09d

--- Units/MMLAddon/updater.pas
+++ Units/MMLAddon/updater.pas
@@ -14,6 +14,8 @@ type
           destructor Destroy; override;
     end;       }
     TMemory = pointer;
+    TFunctionBoolean = function: boolean;
+
 
     TMMLFileDownloader = class(TObject)
          private
@@ -23,23 +25,40 @@ type
            HTTPSend: THTTPSend;
            FFileSize: Integer;
            FDownloaded: Boolean;
-           FOnChange: TProcedure;
+           FOnChange: TFunctionBoolean;
+           FOnBeat: TFunctionBoolean;
 
          public
             constructor Create;
             destructor Destroy; override;
 
+            { Download and Save the file }
             function DownloadAndSave: Boolean;
+
+            { Replace the new file with the one we downloaded }
             function Replace: Boolean;
 
+            { Where do we get the file from? }
             property FileURL: String read FFileURL write FFileURL;
-            property ReplacementFile: String read FReplacementFile write FReplacementFile;
-            property Downloaded: Boolean read FDownloaded;
-            property OnChange: TProcedure read FOnChange write FOnChange;
 
+            { The file to write to. Note that downloading downloads to this
+              file with a '_' added to the end of the FileName;
+              Replace; is called to replace the old file with the downloaded
+              one }
+            property ReplacementFile: String read FReplacementFile write FReplacementFile;
+
+            { Return true if we have downloaded the file completely }
+            property Downloaded: Boolean read FDownloaded;
+
+            { If either of these events return "True", an exception is thrown
+              and the download is cancelled. This way we can easily `cancel'
+              a download. }
+            property OnChange: TFunctionBoolean read FOnChange write FOnChange;
+
+            { Called every 50ms }
+            property OnBeat: TFunctionBoolean read FOnBeat write FOnBeat;
 
             function GetPercentage: Integer;
-
          private
             procedure TryToGetFileSize;
 
@@ -47,11 +66,7 @@ type
                       const Buffer: TMemory; Len: Integer);
             procedure OnStatus(Sender: TObject; Reason: THookSocketReason;
                       const Value: String);
-
-
-
-
-
+            procedure OnHeartBeat(Sender: TObject);
     end;
 
 
@@ -82,7 +97,15 @@ begin
   if FFileSize = 0 then
     TryToGetFileSize;
   if Assigned(FOnChange) then
-    FOnChange();
+    if FOnChange() then
+      raise Exception.Create('OnChange event called for a quit');
+end;
+
+procedure TMMLFileDownloader.OnHeartBeat(Sender: TObject);
+begin
+  if Assigned(FOnBeat) then
+    if FOnBeat() then
+      raise Exception.Create('OnChange event called for a quit');
 end;
 
 procedure TMMLFileDownloader.OnMonitor(Sender: TObject; Writing: Boolean;
@@ -97,7 +120,8 @@ begin
   if FFileSize = 0 then
     TryToGetFileSize;
   if Assigned(FOnChange) then
-    FOnChange();
+    if FOnChange() then
+      raise Exception.Create('OnChange event called for a quit');
  // writeln('Percent done: ' + IntToStr(GetPercentage));
 end;
 
@@ -117,9 +141,13 @@ var
   f: TFileStream;
 
 begin
+  Result := False;
   HTTPSend := THTTPSend.Create;
   HTTPSend.Sock.OnMonitor:=@Self.OnMonitor;
   HTTPSend.Sock.OnStatus:=@Self.OnStatus;
+  HTTPSend.Sock.OnHeartbeat:=@Self.OnHeartBeat;
+  HTTPSend.Sock.HeartbeatRate:=50;
+
   if FReplacementFile = '' then
     raise Exception.Create('ReplacementFile not set');
   if FileURL = '' then
@@ -135,7 +163,8 @@ begin
       Response.CopyFrom(HTTPSend.Document, 0);
     end;
     FDownloaded := True;
-  finally
+  except
+    writeln('DownloadAndSave: Exception Occured');
     HTTPSend.Free;
     Response.Free;
   end;
