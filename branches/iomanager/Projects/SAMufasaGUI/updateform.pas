2425598e576e9b0d0089391d20ab9083f219811c
BenLand100 <BenLand100@3f818213-9676-44b0-a9b4-5e4c4e03d09d>
2010-01-20 18:20:09 +0000

    Creating branch for IOManager work so I don't fuck up the trunk
    git-svn-id: http://www.villavu.com/repositories/merlijn/mufasa@431 3f818213-9676-44b0-a9b4-5e4c4e03d09d

---
+++ branches/iomanager/Projects/SAMufasaGUI/updateform.pas
@@ -0,0 +1,241 @@
+unit updateform;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
+  ComCtrls, StdCtrls, updater;
+
+type
+
+  { TSimbaUpdateForm }
+
+  { TSimbaVersionThread }
+
+  TSimbaVersionThread = class(TThread)
+  public
+    ResultStr : string;
+    InputURL : string;
+    Done : boolean;
+    procedure Execute; override;
+  end;
+  TSimbaUpdateForm = class(TForm)
+    DownloadSpeed: TLabel;
+    UpdateLog: TMemo;
+    UpdateButton: TButton;
+    OkButton: TButton;
+    CancelButton: TButton;
+    DownloadProgress: TProgressBar;
+    procedure CancelButtonClick(Sender: TObject);
+    procedure CleanUpdateForm(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure OkButtonClick(Sender: TObject);
+    procedure UpdateButtonClick(Sender: TObject);
+    function CanUpdate: Boolean;
+
+  private
+    { private declarations }
+
+    Updater: TMMLFileDownloader;
+    FStartTime : longword;
+    FCancelling: Boolean;
+    FDone: Boolean;
+    FUpdating : boolean;
+    FOldSpeed : integer;
+    FLastUpdateSpeed : longword;
+    FSimbaVersion: Integer;
+    SimbaVersionThread : TSimbaVersionThread;
+  private
+    function OnUpdateBeat: Boolean;
+    function GetLatestSimbaVersion: Integer;
+  public
+    { public declarations }
+    procedure PerformUpdate;
+  protected
+    FCancelled: Boolean;
+  end; 
+
+const
+  DownloadSpeedTextRunning = 'Downloading at %d kB/s';
+  DownloadSpeedTextEnded = 'Downloaded at %d kB/s';
+
+var
+  SimbaUpdateForm: TSimbaUpdateForm;
+
+implementation
+
+uses
+  internets,  TestUnit, simbasettings,lclintf;
+
+function TSimbaUpdateForm.CanUpdate: Boolean;
+begin
+  GetLatestSimbaVersion;
+  Writeln(format('Current Simba version: %d',[TestUnit.SimbaVersion]));
+  Writeln('Latest Simba Version: ' + IntToStr(FSimbaVersion));
+  Exit(testunit.SimbaVersion < FSimbaVersion);
+end;
+
+function TSimbaUpdateForm.GetLatestSimbaVersion: Integer;
+
+begin
+  if SimbaVersionThread = nil then//Create thread (only if no-other one is already running)
+  begin
+    SimbaVersionThread := TSimbaVersionThread.Create(true);
+
+    SimbaVersionThread.InputURL := SettingsForm.Settings.GetSetLoadSaveDefaultKeyValueIfNotExists(
+                'Settings/Updater/RemoteVersionLink',
+                'http://old.villavu.com/merlijn/Simba'{$IFDEF WINDOWS} +
+                '.exe'{$ENDIF} + '.version',
+                SimbaSettingsFile);
+
+    SimbaVersionThread.Resume;
+    while SimbaVersionThread.Done = false do//Wait till thread is done
+    begin
+      Application.ProcessMessages;
+      Sleep(50);
+    end;
+    FSimbaVersion := StrToIntDef(Trim(SimbaVersionThread.ResultStr), -1);//Read output
+    FreeAndNil(SimbaVersionThread);//Free the thread
+  end else
+  begin
+    //Another thread is already running, lets wait for it! (When it's nil, it means that the result is written!)
+    while SimbaVersionThread = nil do
+    begin;
+      Application.ProcessMessages;
+      Sleep(50);
+    end;
+  end;
+  Exit(FSimbaVersion);
+end;
+
+procedure TSimbaUpdateForm.UpdateButtonClick(Sender: TObject);
+begin
+  if FUpdating then
+    UpdateLog.Lines.Add('Already performing an update!')
+  else
+    Self.PerformUpdate;
+end;
+
+procedure TSimbaUpdateForm.CancelButtonClick(Sender: TObject);
+begin
+  if FCancelled or FDone then
+  begin
+    Self.ModalResult:=mrCancel;
+    Self.Hide;
+  end else
+  begin
+    FCancelling := True;
+  end;
+end;
+
+procedure TSimbaUpdateForm.CleanUpdateForm(Sender: TObject);
+begin
+  Self.DownloadProgress.Position:=0;
+  Self.UpdateLog.Clear;
+  Self.UpdateLog.Lines.Add('---------- Update Session ----------');
+  Self.DownloadSpeed.Visible:= false;
+end;
+
+procedure TSimbaUpdateForm.FormCreate(Sender: TObject);
+begin
+  FDone := True;
+  FUpdating:= false;
+
+end;
+
+procedure TSimbaUpdateForm.OkButtonClick(Sender: TObject);
+begin
+  Self.ModalResult:=mrOK;
+  Self.Hide;
+end;
+
+{ Return true if we have to cancel }
+function TSimbaUpdateForm.OnUpdateBeat: Boolean;
+var
+  Percentage: Integer;
+  NewSpeed : integer;
+begin
+  Application.ProcessMessages;
+
+  Percentage := Updater.GetPercentage();
+  if Percentage <> -1 then
+    DownloadProgress.Position:=Percentage;
+  // Formula for speed (kB/s) -> (Bytes div 1000) / (MSecSinceStart div 1000) = Bytes/ MSecSinceStart
+  NewSpeed :=(Updater.DownloadedSize)  div ((GetTickCount-FStartTime));
+  if abs(NewSpeed - FOldSpeed) > 1 then
+    if (GetTickCount - FLastUpdateSpeed) > 1000 then    //Only update the speed text every second
+    begin;
+      FOldSpeed:= NewSpeed;
+      DownloadSpeed.Caption:= Format(DownloadSpeedTextRunning,[NewSpeed]);
+      FLastUpdateSpeed:= GetTickCount;
+    end;
+  Result := FCancelling;
+end;
+
+procedure TSimbaUpdateForm.PerformUpdate;
+
+begin
+  FUpdating:= True;
+  Updater := TMMLFileDownloader.Create;
+
+  FDone := False;
+  FCancelling := False;
+  FCancelled := False;
+
+  Updater.FileURL := SettingsForm.Settings.GetSetLoadSaveDefaultKeyValueIfNotExists(
+        'Settings/Updater/RemoteLink',
+        'http://old.villavu.com/merlijn/Simba'{$IFDEF WINDOWS} +'.exe'{$ENDIF},
+        SimbaSettingsFile
+  );
+
+  //ApplicationName{$IFDEF WINDOWS} +'.exe'{$ENDIF};
+
+  // Should work on Windows as well
+  Updater.ReplacementFile := ExtractFileName(Application.ExeName);
+  Updater.OnBeat := @Self.OnUpdateBeat;
+  Updater.BasePath := ExtractFilePath(Application.ExeName);
+
+  Self.UpdateLog.Lines.Add('Starting download of ' + Updater.FileURL + ' ...');
+  try
+    Self.OkButton.Enabled := False; // grey out button
+    DownloadSpeed.Visible:= true;
+    DownloadSpeed.Caption:= Format(DownloadSpeedTextRunning,[0]);
+    FStartTime:= GetTickCount - 1;//Be sure that we don't get div 0
+    Updater.DownloadAndSave;
+    DownloadSpeed.Caption := Format(DownloadSpeedTextEnded,[Updater.FileSize div (GetTickCount-FStartTime)]);
+    Self.UpdateLog.Lines.Add('Downloaded to ' + Updater.ReplacementFile + '_ ...');
+    Updater.Replace;
+    Self.UpdateLog.Lines.Add('Renaming ' + Updater.ReplacementFile + ' to ' + Updater.ReplacementFile + '_old_');
+    Self.UpdateLog.Lines.Add('Renaming ' + Updater.ReplacementFile + '_ to ' + Updater.ReplacementFile);
+    Self.UpdateLog.Lines.Add('Deleting ' + Updater.ReplacementFile + '_old_');
+    Updater.Free;
+    Self.UpdateLog.Lines.Add('Done ... ');
+    Self.UpdateLog.Lines.Add('Please restart all currently running Simba binaries.');
+  except
+    FCancelling := False;
+    FCancelled := True;
+    DownloadSpeed.Visible:= false;
+    Self.UpdateLog.Lines.Add('Download stopped at '+inttostr(DownloadProgress.Position)+'%... Simba did not succesfully update.');
+    // more detailed info
+    writeln('EXCEPTION IN UPDATEFORM: We either hit Cancel, or something went wrong with files');
+  end;
+  FDone := True;
+  Self.OkButton.Enabled := True; // un-grey out button
+  FUpdating:= false;
+end;
+
+{ TSimbaVersionThread }
+
+procedure TSimbaVersionThread.Execute;
+begin
+  ResultStr:= GetPage(InputURL);
+  done := true;
+end;
+
+initialization
+  {$I updateform.lrs}
+
+end.
+
---
+++ branches/iomanager/Units/MMLCore/client.pas
@@ -0,0 +1,86 @@
+{
+	This file is part of the Mufasa Macro Library (MML)
+	Copyright (c) 2009 by Raymond van Venetië and Merlijn Wajer
+
+    MML is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MML is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MML.  If not, see <http://www.gnu.org/licenses/>.
+
+	See the file COPYING, included in this distribution,
+	for details about the copyright.
+
+    Client class for the Mufasa Macro Library
+}
+
+
+unit Client;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, MufasaTypes,
+  Window, Input, Files, Finder, Bitmaps, dtm, ocr;
+
+{
+TClient is a full-blown instance of the MML.
+It binds all the components together.
+}
+
+type
+    TClient = class(TObject)
+        constructor Create;
+        destructor Destroy; override;
+
+        public
+            MWindow: TMWindow;
+            MInput: TMInput;
+            MFiles: TMFiles;
+            MFinder: TMFinder;
+            MBitmaps : TMBitmaps;
+            MDTM: TMDTM;
+            MOCR: TMOCR;
+
+    end;
+
+implementation
+
+// Possibly pass arguments to a default window.
+constructor TClient.Create;
+begin
+  inherited Create;
+
+  MWindow := TMWindow.Create;
+  MInput := TMInput.Create(MWindow);
+  MFiles := TMFiles.Create;
+  MFinder := TMFinder.Create(Self);
+  MBitmaps := TMBitmaps.Create(self);
+  MDTM := TMDTM.Create(self);
+  MOCR := TMOCR.Create(self);
+end;
+
+destructor TClient.Destroy;
+begin
+  MOCR.Free;
+  MDTM.Free;
+  MBitmaps.Free;
+  MFinder.Free;
+  MFiles.Free;
+  MInput.Free;
+  MWindow.Free;
+
+  inherited;
+end;
+
+end.
+
