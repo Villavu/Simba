d6474cda3de403a7694d08a57186b732bb7140f9
BenLand100 <BenLand100@3f818213-9676-44b0-a9b4-5e4c4e03d09d>
2010-01-20 18:20:09 +0000

    Creating branch for IOManager work so I don't fuck up the trunk
    git-svn-id: http://www.villavu.com/repositories/merlijn/mufasa@431 3f818213-9676-44b0-a9b4-5e4c4e03d09d

---
+++ branches/iomanager/Projects/forking/project1.lpr
@@ -0,0 +1,121 @@
+program project1;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Classes, SysUtils, CustApp, Process
+  { you can add units after this };
+
+type
+
+  { TMyApplication }
+
+  TMyApplication = class(TCustomApplication)
+  protected
+    procedure DoRun; override;
+  public
+    constructor Create(TheOwner: TComponent); override;
+    destructor Destroy; override;
+    procedure WriteHelp; virtual;
+  end;
+
+{ TMyApplication }
+
+procedure TMyApplication.DoRun;
+var
+  ErrorMsg: String;
+
+  Processes: array of TProcess;
+  i, c, a:integer;
+
+begin
+
+  // quick check parameters
+  ErrorMsg:=CheckOptions('h','help');
+  if ErrorMsg<>'' then begin
+    ShowException(Exception.Create(ErrorMsg));
+    Terminate;
+    Exit;
+  end;
+
+  // parse parameters
+  if HasOption('h','help') then begin
+    WriteHelp;
+    Terminate;
+    Exit;
+  end;
+
+  a := 5;
+  SetLength(Processes, a);
+  for i := 0 to a - 1 do
+  begin
+    Processes[i] := TProcess.Create(Self);
+    {$WARNING SET THIS PATH}
+    //Processes[i].CommandLine := 'C:/mufasa/Projects/MufasaTests/project1.exe';
+    Processes[i].CommandLine := '/home/merlijn/Programs/mufasa/Projects/MufasaTests/project1';
+    //Processes[i].ApplicationName := 'ForkingTest: ' + inttostr(i);
+    Processes[i].Execute;
+    sleep(100);
+    Writeln('Process ' + inttostr(i) + ', has ID ' + inttostr(Processes[i].ProcessID));
+  end;
+
+  sleep(10000);
+  while true do
+  begin
+    c := 0;
+    for i := 0 to a - 1 do
+    begin
+      if not Processes[i].Running then
+        c := c + 1;
+    end;
+    if c = a then
+      break;
+    sleep(1000);
+  end;
+
+  for i := 0 to a - 1 do
+  begin
+    Processes[i].Free;
+  end;
+  { add
+
+  end;
+  your program here }
+
+  // stop program loop
+  writeln('done');
+  Terminate;
+end;
+
+constructor TMyApplication.Create(TheOwner: TComponent);
+begin
+  inherited Create(TheOwner);
+  StopOnException:=True;
+end;
+
+destructor TMyApplication.Destroy;
+begin
+  inherited Destroy;
+end;
+
+procedure TMyApplication.WriteHelp;
+begin
+  { add your help code here }
+  writeln('Usage: ',ExeName,' -h');
+end;
+
+var
+  Application: TMyApplication;
+
+{$IFDEF WINDOWS}{$R project1.rc}{$ENDIF}
+
+begin
+  Application:=TMyApplication.Create(nil);
+  Application.Title:='My Application';
+  Application.Run;
+  Application.Free;
+end.
+
---
+++ branches/iomanager/Units/MMLCore/fontloader.pas
@@ -0,0 +1,226 @@
+{
+	This file is part of the Mufasa Macro Library (MML)
+	Copyright (c) 2009 by Raymond van Venetië and Merlijn Wajer
+
+    MML is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MML is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MML.  If not, see <http://www.gnu.org/licenses/>.
+
+	See the file COPYING, included in this distribution,
+	for details about the copyright.
+
+    Fonts class for the Mufasa Macro Library
+}
+
+unit fontloader;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils,
+  ocrutil; // contains the actual `loading'
+
+{
+  We will not give any access to actual indices.
+}
+
+type
+    TMFont = class(TObject)
+            constructor Create;
+            destructor Destroy; override;
+
+            function Copy: TMFont;
+        public
+            Name: String;
+            Data: TOcrData;
+    end;
+
+
+type
+    TMFonts = class(TObject)
+            constructor Create;
+            destructor Destroy; override;
+
+            function GetFont(Name: String): TOcrData;
+            function FreeFont(Name: String): boolean;
+            function LoadFont(Name: String; Shadow: Boolean): boolean;
+            procedure SetPath(aPath: String);
+            function GetPath: String;
+
+            function Copy: TMFonts;
+        private
+            function GetFontIndex(Name: String): Integer;
+        private
+            Fonts: TList;
+            Path: String;
+        end;
+
+implementation
+
+uses
+  files, MufasaTypes;
+
+
+constructor TMFont.Create;
+begin
+  inherited;
+
+  Name:='';
+end;
+
+destructor TMFont.Destroy;
+begin
+  Name:='';
+
+  inherited;
+end;
+
+function TMFont.Copy: TMFont;
+var
+  i, l, ll:integer;
+begin
+  Result := TMFont.Create;
+  Result.Name := Self.Name;
+  Move(Self.Data.ascii[0], Self.Data.ascii[0], length(Self.Data.ascii) * SizeOf(Integer));
+  l := Length(Self.Data.Pos);
+  SetLength(Result.Data.pos, l);
+  for i := 0 to l - 1 do
+  begin
+    ll := length(Self.Data.Pos[i]);
+    setlength(Result.Data.Pos[i], ll);
+    Move(Self.Data.Pos[i][0], Result.Data.Pos[i][0], ll*SizeOf(Integer));
+  end;
+
+  SetLength(Result.Data.pos_adj,  length(Self.Data.pos_adj));
+  Move(Self.Data.pos_adj[0], Result.Data.pos_adj[0], length(Self.Data.pos_adj) * SizeOf(real));
+
+  l := Length(Self.Data.neg);
+  SetLength(Result.Data.neg, l);
+  for i := 0 to l - 1 do
+  begin
+    ll := length(Self.Data.neg[i]);
+    setlength(Result.Data.neg[i], ll);
+    Move(Self.Data.neg[i][0], Result.Data.neg[i][0], ll*SizeOf(Integer));
+  end;
+
+  SetLength(Result.Data.neg_adj,  length(Self.Data.neg_adj));
+  Move(Self.Data.neg_adj[0], Result.Data.neg_adj[0], length(Self.Data.neg_adj) * SizeOf(real));
+
+  SetLength(Result.Data.map,  length(Self.Data.map));
+  Move(Self.Data.map[0], Result.Data.map[0], length(Self.Data.map) * SizeOf(char));
+
+  Result.Data.Width := Self.Data.Width;
+  Result.Data.Height := Self.Data.Height;
+  Result.Data.inputs := Self.Data.inputs;
+  Result.Data.outputs := Self.Data.outputs;
+end;
+
+constructor TMFonts.Create;
+
+begin
+  inherited;
+
+  Fonts := TList.Create;
+end;
+
+destructor TMFonts.Destroy;
+var
+  i:integer;
+begin
+  for i := 0 to Fonts.Count - 1 do
+    TMFont(Fonts.Items[i]).Free;
+  Fonts.Free;
+
+  inherited;
+end;
+
+procedure TMFonts.SetPath(aPath: String);
+begin
+  Path := aPath;
+end;
+
+function TMFonts.GetPath: String;
+begin
+  Exit(Path);
+end;
+
+function TMFonts.GetFontIndex(Name: String): Integer;
+var
+  i: integer;
+begin
+  for i := 0 to Fonts.Count - 1 do
+  begin
+    if Name = TMFont(Fonts.Items[i]).Name then
+      Exit(i);
+  end;
+  raise Exception.Create('Font [' + Name + '] not found.');
+  Exit(-1);
+end;
+
+function TMFonts.GetFont(Name: String): TOcrData;
+var
+  i: integer;
+begin
+  i := GetFontIndex(Name);
+  Exit(TMFont(Fonts.Items[i]).Data);
+end;
+
+function TMFonts.FreeFont(Name: String): boolean;
+var
+  i: integer;
+begin
+  i := GetFontIndex(Name);
+  TMFont(Fonts.Items[i]).Free;
+  Fonts.Delete(i);
+end;
+
+function TMFonts.LoadFont(Name: String; Shadow: Boolean): boolean;
+var
+  f: TMFont;
+  ocrdata: TOcrData;
+
+begin
+  if not DirectoryExists(Path + Name) then
+  begin
+    raise Exception.Create('LoadFont: Directory ' + Path + Name + ' does not exists.');
+    Exit(False);
+  end;
+
+  ocrdata := InitOCR(Path + Name + DS, Shadow);
+
+  f:=TMFont.Create;
+  f.Name := Name;
+  if Shadow then
+    F.Name := F.Name + '_s';
+  f.Data := ocrdata;
+
+  Fonts.Add(f);
+  {$IFDEF FONTDEBUG}
+  writeln('Loaded Font ' + f.Name);
+  {$ENDIF}
+end;
+
+function TMFonts.Copy: TMFonts;
+
+var
+  i:integer;
+begin
+  Result := TMFonts.Create;
+  Result.Path := Self.GetPath();
+  for i := 0 to Self.Fonts.Count -1 do
+    Result.Fonts.Add(TMFont(Self.Fonts.Items[i]).Copy());
+end;
+
+end.
+
