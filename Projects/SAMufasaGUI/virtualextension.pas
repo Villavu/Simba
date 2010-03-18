bf52f1fe4037d8f41c89e7327e35f1233cd76a87
Merlijn Wajer <merlijn@wajer.org>
2010-03-19 00:31:39 +0100

    Initial hooks code

--- Projects/SAMufasaGUI/virtualextension.pas
+++ Projects/SAMufasaGUI/virtualextension.pas
@@ -22,6 +22,28 @@ type
 
     end;
 
+
+    { Each hook has a HookName which the extension uses to identify the hook.
+      Additionally, variables are exported. Currently we will use an Array of Variant.
+      Therefore, we will have to specify what amount Arguments we will pass.
+      ( So it is not type safe... Not like Variants are type safe... )
+    }
+    type TEventHook = record
+         HookName: String;
+         ArgumentCount: Integer;
+    end;
+
+var
+    EventHooks: Array [0..7] of TEventHook =
+      (    (HookName : 'onScriptCompile' ; ArgumentCount : 1),
+	   (HookName : 'onScriptStart'   ; ArgumentCount : 1),
+	   (HookName : 'onScriptPause'   ; ArgumentCount : 1),
+	   (HookName : 'onScriptStop'    ; ArgumentCount : 1),
+	   (HookName : 'onColourPick'    ; ArgumentCount : 3),
+	   (HookName : 'onOpenFile'      ; ArgumentCount : 1),
+	   (HookName : 'onOpenSocket'    ; ArgumentCount : 1),
+	   (HookName : 'onWriteFile'     ; ArgumentCount : 1));
+
 implementation
 
 end.
