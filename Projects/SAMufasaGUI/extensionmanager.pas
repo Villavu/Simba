9dcb5fa7dbb87deb5f8e7dfe3b672fd89ddf76b9
Merlijn Wajer <merlijn@wajer.org>
2010-03-19 23:11:26 +0100

    More additions to Extensions

--- Projects/SAMufasaGUI/extensionmanager.pas
+++ Projects/SAMufasaGUI/extensionmanager.pas
@@ -18,21 +18,46 @@ type
     public
            constructor Create;
            destructor Destroy; override;
+    private
+           Extensions: TList;
+    public
+           function HandleHook(HookName: String; Args: Array of Variant): Variant;
     end;
 
 
 
 implementation
+uses
+  pseventextension, virtualextension;
 
 
 constructor TExtensionManager.Create;
 begin
-
+  Extensions := TList.Create;
 end;
 
 destructor TExtensionManager.Destroy;
+var
+  i: Integer;
 begin
+  {
+  for i := 0 to Extensions.Count - 1 do
+    TVirtualSimbaExtension(Extensions.Items[i]).Free;
+  }
+  Extensions.Free;
+end;
 
+// How do we return more than one result?
+function TExtensionManager.HandleHook(HookName: String; Args: Array of Variant): Variant;
+var
+  i: Integer;
+begin
+  for i := 0 to Extensions.Count -1 do
+    if TVirtualSimbaExtension(Extensions.Items[i]).HookExists(HookName) then
+      if TVirtualSimbaExtension(Extensions.Items[i]).ExecuteHook(HookName, Args, Result) <> 0 then
+      begin
+        // Not succesfull.
+      end;
 end;
 
 end.
