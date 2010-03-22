c112ad902ea4e407dcb69670ea7abf8871d27130
Raymond <Raymond@.(none)>
2010-03-22 15:58:30 +0100

    Uhm ScriptSetProp and ScriptGetProp now work with an Enum, ctrl + alt + s is a global shorcut (on windows). f2 is a non-local shortcut to stop the script. Console now hides without losing the information, changed compiler setting for windows, added define mDebug for if you want a more extensive debug.
    Former-commit-id: 51b3c3cf9450471f7e2cba5fa1ef61ab69c9141b

--- Units/MMLAddon/scriptproperties.pas
+++ Units/MMLAddon/scriptproperties.pas
@@ -7,14 +7,31 @@ interface
 uses
   Classes, SysUtils;
 type
+
+    { TScriptProperties }
+
+    TSP_Property = (
+      SP_WriteTimeStamp //Writes the time infront of a writeln
+    );
+    TSP_Properties = set of TSP_Property;
+
+
     TScriptProperties = class(TObject)
+    private
+      FProperties : TSP_Properties;
+      FWriteTimeStamp : boolean;
     public
-            constructor Create;
-            destructor Destroy; override;
-            function SetProp(Name: String; Value: String): Boolean;
+      constructor Create;
+      destructor Destroy; override;
+      function GetProperty(name: ansistring; var Prop : TSP_Property): boolean;
+      function GetProp(name: ansistring; var Value : ansistring) : Boolean;overload;
+      function GetProp(Prop : TSP_Property; var Value : ansistring) : Boolean;overload;
+      function SetProp(Name: ansistring; Value: ansistring): Boolean;overload;
+      function SetProp(Prop : TSP_Property; Value: ansistring): Boolean;overload;
     public
-            WriteTimeStamp: Boolean;
+      property WriteTimeStamp : boolean read FWriteTimeStamp;
     end;
+
 
 implementation
 
@@ -22,7 +39,8 @@ constructor TScriptProperties.Create;
 begin
   inherited;
 
-  WriteTimeStamp := False;
+  FWriteTimeStamp := False;
+  FProperties := [];
 
   { set default values }
 end;
@@ -32,21 +50,61 @@ begin
   inherited Destroy;
 end;
 
-function TScriptProperties.SetProp(Name: String; Value: String): Boolean;
-
+function TScriptProperties.GetProperty(name: ansistring; var Prop : TSP_Property): boolean;
+const
+  Names : array[TSP_Property] of ansistring = ('writetimestamp');
+var
+  i : integer;
 begin
-  {
-    Fucking hell. We can't use a String in case statement, and we cannot define
-    hash maps as constants, and we also cannot use variables in case statements.
-  }
-  Name := LowerCase(Name);
-  if Name = 'writetimestamp' then
-  begin
-    WriteTimeStamp := LowerCase(Value) = 'true';
-    Exit(True);
+  Result := false;
+  for i := 0 to length(names)- 1 do
+    if lowercase(name) = Names[TSP_Property(i)] then
+    begin
+      Prop := (TSP_Property(i));
+      Exit(true);
+    end;
+end;
+
+function TScriptProperties.GetProp(name: ansistring; var Value: ansistring): Boolean;
+var
+  Prop : TSP_Property;
+begin
+  Result := false;
+  if GetProperty(name,prop) then
+    Result := (GetProp(Prop,value));
+end;
+
+function TScriptProperties.GetProp(Prop: TSP_Property; var Value: ansistring
+  ): Boolean;
+begin
+  Result := true;
+  case Prop of
+    SP_WriteTimeStamp : Value := BoolToStr(Prop in FProperties,true);
   end;
-  {more if bla }
-  Result := False;
+end;
+
+function TScriptProperties.SetProp(Name: ansistring; Value: ansistring): Boolean;
+var
+  Prop : TSP_Property;
+begin
+  Result := false;
+  if GetProperty(name,prop) then
+    Result := (SetProp(Prop,value));
+end;
+
+function TScriptProperties.SetProp(Prop: TSP_Property; Value: ansistring): Boolean;
+begin
+  case Prop of
+    SP_WriteTimeStamp : if lowercase(value) = 'true' then
+                     begin
+                       FWriteTimeStamp:= True;
+                       FProperties := FProperties + [Prop];
+                     end else
+                     begin
+                       FWriteTimeStamp := False;
+                       FProperties := FProperties - [Prop];
+                     end;
+  end;
 end;
 
 end.
