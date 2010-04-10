ece3bb350231f94a10273b8c8ae19bfe73690ec4
Raymond <Raymond@.(none)>
2010-04-10 14:34:24 +0200

    Added script property SP_OnTerminate.. This way you can add functions to a OnTerminate event list, these functions will be called at the end of the script.

--- Units/MMLAddon/scriptproperties.pas
+++ Units/MMLAddon/scriptproperties.pas
@@ -5,13 +5,14 @@ unit scriptproperties;
 interface
 
 uses
-  Classes, SysUtils;
+  Classes, SysUtils,MufasaTypes,mufasabase;
 type
 
     { TScriptProperties }
 
     TSP_Property = (
-      SP_WriteTimeStamp //Writes the time infront of a writeln
+      SP_WriteTimeStamp, //Writes the time infront of a writeln
+      SP_OnTerminate
     );
     TSP_Properties = set of TSP_Property;
 
@@ -19,40 +20,50 @@ type
     TScriptProperties = class(TObject)
     private
       FProperties : TSP_Properties;
-      FWriteTimeStamp : boolean;
+      FOnTerminateProcs : TStringList;
+      function HasTimeStamp: boolean;
     public
       constructor Create;
       destructor Destroy; override;
       function GetProperty(name: ansistring; var Prop : TSP_Property): boolean;
-      function GetProp(name: ansistring; var Value : ansistring) : Boolean;overload;
-      function GetProp(Prop : TSP_Property; var Value : ansistring) : Boolean;overload;
-      function SetProp(Name: ansistring; Value: ansistring): Boolean;overload;
-      function SetProp(Prop : TSP_Property; Value: ansistring): Boolean;overload;
+      function GetProp(name: ansistring; var Value : TVariantArray) : Boolean;overload;
+      function GetProp(Prop : TSP_Property; var Value : TVariantArray) : Boolean;overload;
+      function SetProp(Name: ansistring; Value: TVariantArray): Boolean;overload;
+      function SetProp(Prop : TSP_Property; Value: TVariantArray): Boolean;overload;
     public
-      property WriteTimeStamp : boolean read FWriteTimeStamp;
+      property Properties : TSP_Properties read FProperties;
+      property WriteTimeStamp : boolean read HasTimeStamp;
+      property OnTerminateProcs : TStringList read FOnTerminateProcs;
     end;
 
 
 implementation
 
+function TScriptProperties.HasTimeStamp: boolean;
+begin
+  result := (SP_WriteTimeStamp in FProperties);
+end;
+
 constructor TScriptProperties.Create;
 begin
   inherited;
 
-  FWriteTimeStamp := False;
   FProperties := [];
-
+  FOnTerminateProcs := TStringList.Create;
+  FOnTerminateProcs.CaseSensitive:= false;
+  FOnTerminateProcs.Duplicates:= dupIgnore;
   { set default values }
 end;
 
 destructor TScriptProperties.Destroy;
 begin
+  FOnTerminateProcs.Free;
   inherited Destroy;
 end;
 
 function TScriptProperties.GetProperty(name: ansistring; var Prop : TSP_Property): boolean;
 const
-  Names : array[TSP_Property] of ansistring = ('writetimestamp');
+  Names : array[TSP_Property] of ansistring = ('writetimestamp','onterminate');
 var
   i : integer;
 begin
@@ -65,7 +76,7 @@ begin
     end;
 end;
 
-function TScriptProperties.GetProp(name: ansistring; var Value: ansistring): Boolean;
+function TScriptProperties.GetProp(name: ansistring; var Value: TVariantArray): Boolean;
 var
   Prop : TSP_Property;
 begin
@@ -74,16 +85,28 @@ begin
     Result := (GetProp(Prop,value));
 end;
 
-function TScriptProperties.GetProp(Prop: TSP_Property; var Value: ansistring
+function TScriptProperties.GetProp(Prop: TSP_Property; var Value: TVariantArray
   ): Boolean;
+var
+  i : integer;
 begin
   Result := true;
+  Setlength(value,0);
   case Prop of
-    SP_WriteTimeStamp : Value := BoolToStr(Prop in FProperties,true);
+    SP_WriteTimeStamp : begin SetLength(Value,1); Value[0] := BoolToStr(Prop in FProperties,true); end;
+    SP_OnTerminate :
+      begin
+        if not (Prop in FProperties) then
+          exit;
+        setlength(value,FOnTerminateProcs.Count);
+        for i := 0 to high(Value) do
+          value[i] := FOnTerminateProcs[i];
+        result := true;
+      end;
   end;
 end;
 
-function TScriptProperties.SetProp(Name: ansistring; Value: ansistring): Boolean;
+function TScriptProperties.SetProp(Name: ansistring; Value: TVariantArray): Boolean;
 var
   Prop : TSP_Property;
 begin
@@ -92,18 +115,39 @@ begin
     Result := (SetProp(Prop,value));
 end;
 
-function TScriptProperties.SetProp(Prop: TSP_Property; Value: ansistring): Boolean;
+function TScriptProperties.SetProp(Prop: TSP_Property; Value: TVariantArray): Boolean;
+var
+  i : integer;
 begin
+  result := false;
+  if Length(value) < 1 then
+  begin;
+    mDebugLn('SetProp passed a TVarArray with a length of 0' );
+    exit;
+  end;
   case Prop of
-    SP_WriteTimeStamp : if lowercase(value) = 'true' then
-                     begin
-                       FWriteTimeStamp:= True;
-                       FProperties := FProperties + [Prop];
-                     end else
-                     begin
-                       FWriteTimeStamp := False;
-                       FProperties := FProperties - [Prop];
-                     end;
+    SP_WriteTimeStamp : begin
+                          if length(Value) <> 1 then
+                          begin
+                            mDebugLn('SP_WriteTimeStamp only needs 1 value in the array');
+                            exit;
+                          end;
+                          try
+                            if Value[0] = True then
+                              FProperties := FProperties + [Prop]
+                            else
+                              FProperties := FProperties - [Prop];
+                          except
+                            mDebugLn('Could not convert your value passed to SetProp');
+                          end;
+                       end;
+    SP_OnTerminate :
+      begin
+        for i := 0 to high(value) do
+          FOnTerminateProcs.Add(Value[i]);
+        FProperties := FProperties + [prop];
+        Result := True;
+      end;
   end;
 end;
 
