8fef2d19802c177b889e8730bac3d9da7bd9bf3d
Merlijn Wajer <merlijn@wajer.org>
2010-05-19 17:23:32 +0200

    PyMML indentation fix + cleanup.

--- Projects/MMLLib/pymml/mmltypes.py
+++ Projects/MMLLib/pymml/mmltypes.py
@@ -5,8 +5,8 @@ class POINT(Structure):
         POINT represents a point with two variables, x and y.
         POINT is mainly used for Python <-> MML communication.
     '''
-        _fields_ = [('x', c_int),
-                    ('y', c_int)]
+    _fields_ = [('x', c_int),
+                ('y', c_int)]
 
 PPOINT = POINTER(POINT)
 
