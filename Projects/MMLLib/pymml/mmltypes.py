9884284d9ffcfae150a4c03e6cf33889dace158e
Merlijn Wajer <merlijn@wajer.org>
2010-06-01 12:52:32 +0200

    Array passing works, with copy overhead.
    Former-commit-id: a1036418976ccea25932ef6986e6f497843fa8c4

--- Projects/MMLLib/pymml/mmltypes.py
+++ Projects/MMLLib/pymml/mmltypes.py
@@ -8,6 +8,33 @@ class POINT(Structure):
     _fields_ = [('x', c_int),
                 ('y', c_int)]
 
+class PascalArray(object):
+    def __init__(self, pastype, ptr, MC):
+        self._type = pastype
+        self._p = ptr
+        self._mc = MC
+
+    def __del__(self):
+        self._mc.dll.fpc_freemem_(self._p)
+
+    def __len__(self):
+        return cast(self._p, POINTER(c_ulong))[0]
+
+    def __getitem__(self, pos):
+        if pos > len(self):
+            print 'Out of range'
+            return None
+        return cast(self._p, POINTER(self._type))[pos+1]
+    
+    def __setitem__(self, pos, item):
+        if pos > len(self):
+            print 'Out of range'
+            return
+        if sizeof(item) != sizeof(self._type):
+            print 'Incorrect structure'
+            return
+        cast(self._p, POINTER(self._type))[pos] = item
+
 PPOINT = POINTER(POINT)
 PINTEGER = POINTER(c_int)
 
