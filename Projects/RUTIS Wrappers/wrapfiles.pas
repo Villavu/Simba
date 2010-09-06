7236770e84ea6fa41d54eabccfb88df307c94fb3
Raymond <Raymond@.(none)>
2010-09-06 22:32:04 +0200

    Leet-alized the wrapper app.
    Former-commit-id: 5d4884e5cdb70ab6da672d77577b5e48ceb448fb

--- Projects/RUTIS Wrappers/wrapfiles.pas
+++ Projects/RUTIS Wrappers/wrapfiles.pas
@@ -45,6 +45,7 @@ procedure TWrapFilesForm.FileButtonClick(Sender: TObject);
 begin
   if FileDialog.Execute then
   begin
+    SaveDirEdit.Directory := ExtractFileDir(FileDialog.FileName);
     FileBox.Items.AddStrings(FileDialog.Files);
   end;
 end;
@@ -61,21 +62,56 @@ procedure TWrapFilesForm.wrpBtnClick(Sender: TObject);
 
 var
   i : integer;
-  Input, Output : TFileStream;
+  Input, Output : TStringList;
+  NewFile : string;
+  YesToAll, NoToAll : boolean;
 begin
+  YesToAll:= false;
+  NoToAll:= false;
   if not DirectoryExists(SaveDirEdit.Directory) then
   begin
-    Writeln(format('Dir %s does not exist',[SaveDirEdit.Directory]));
+    dbg(format('Dir %s does not exist',[SaveDirEdit.Directory]));
     exit;
   end;
-  if not FileBox.Items.Count < 1 then
+  if FileBox.Items.Count < 1 then
   begin
-    Writeln('No files loaded');
+    dbg('No files loaded');
     exit;
   end;
   for i := 0 to FileBox.Items.Count - 1 do
   begin
-
+    if not FileExists(filebox.items[i]) then
+    begin
+      dbg(format('File[%s] does not exist',[FileBox.items[i]]));
+      continue;
+    end;
+    NewFile := SaveDirEdit.Directory + DirectorySeparator + ExtractFileName(FileBox.Items[i]);
+    if not YesToAll and FileExists(NewFile) then
+    begin
+      if NoToAll then
+        Continue;
+      case MessageDlg('File already exists',Format('Do you want to overwrite the file %s',[NewFile]),
+                      mtConfirmation,[mbYes,mbYesToAll,mbNo,mbNoToAll],0) of
+        mrNo : Continue;
+        mrNoToAll : begin
+                      NoToAll:= True;
+                      Continue;
+                    end;
+        mrYesToAll : YesToAll:= true;
+      end;
+    end;
+    dbg(NewFile);
+    try
+      Input := TStringList.Create;
+      Input.LoadFromFile(filebox.items[i]);
+      Output :=  TStringList.Create;
+      ConvertRT(Input,dbgMemo.Lines,Output);
+      Output.SaveToFile(NewFile);
+      Input.free;
+      Output.free;
+    except
+      dbg('Something went wrong');
+    end;
   end;
 end;
 
