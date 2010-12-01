839f4345d3c920c816a6ed6d2b52a69a5505e937
Merlijn Wajer <merlijn@wajer.org>
2010-12-01 21:38:20 +0100

    New MML structure + additions.
    Former-commit-id: d7d74723865fbbe27a5a245d1b600eb056635b46

---
+++ Projects/libmml/pymml/mml/__main__.py
@@ -0,0 +1,38 @@
+from __init__ import *
+
+if __name__ == '__main__':
+    DLL = MMLCore('../libmml.so')
+
+    client = DLL.dll.create_client()
+    print 'Python Client: %d' % client
+    if client in (0, 1):
+        raise Exception('Could create a client');
+
+    c = Color(DLL, client)
+
+
+    ret = c.find((0, 0, 100, 100), 0)
+    print ret
+
+    ret = c.findAll((0, 0, 100, 100), 0, tol=100)
+    print ret
+
+    m = Mouse(DLL, client)
+    
+   
+    print m[(Mouse.Pos, Mouse.Left, Mouse.Right)]
+    m[(Mouse.Pos, Mouse.Right)] = ((300,300), True)
+
+    print m.getButtonStates()
+    sleep(0.5)
+    m.setPos((200,200))
+   
+    sleep(2)
+    print 'Done'
+
+    m[(Mouse.Left, Mouse.Right, Mouse.Middle)] = [False for x in range(3)]
+    for v in zip((Mouse.Left, Mouse.Right), m[(Mouse.Left, Mouse.Right)]):
+        print v
+    print m.getPos()
+    
+    del DLL
