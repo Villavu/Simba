3874776aabad994ca38a413a69c8a1529a108e5d
John P (Dgby714) <Dgby714@aol.com>
2010-12-18 14:38:29 -0500

    Simba: Socket Support.

--- Units/MMLAddon/internets.pas
+++ Units/MMLAddon/internets.pas
@@ -5,14 +5,12 @@ unit internets;
 interface
 
 uses
-  Classes, SysUtils,httpsend,MufasaTypes;
+  Classes, SysUtils, httpsend, blcksock, MufasaTypes, math;
 
 function GetPage(URL: String): String;
 
 type
-
   { THTTPClient }
-
   THTTPClient = class(TObject)
   private
     HTTPSend : THTTPSend;
@@ -32,6 +30,7 @@ type
     constructor Create(Owner : TObject; HandleCookies : boolean = true);
     destructor Destroy;override;
   end;
+
   { TMInternet }
   TMInternet = class(TObject)
   protected
@@ -46,6 +45,41 @@ type
     procedure FreeHTTPClient(Index: Integer);
     constructor Create(Owner : TObject);
     destructor Destroy;override;
+  end;
+
+  { TSock }
+  TSock = class(TObject)
+  private
+    Sock: TTCPBlockSocket;
+    Timeout: integer;
+    Client: TObject;
+  public
+    function RecvBufferStr(Length: integer): string;
+    function RecvString: string;
+    function Recv: string;
+    procedure Send(Data: string);
+    procedure Connect(IP, Port: string);
+    procedure Close;
+    procedure SetTimeout(Time: integer);
+    procedure Bind(IP, Port: string);
+    procedure Listen;
+    function Accept: TTCPBlockSocket;
+    constructor Create(Owner: TObject; Socket: TTCPBlockSocket = nil);
+    destructor Destroy; override;
+  end;
+
+  { TSocks }
+  TSocks = class(TObject)
+  protected
+    Client: TObject;
+    SockList: TList;
+  public
+    function CreateSocket: integer;
+    function CreateSocketEx(Socket: TTCPBlockSocket): integer;
+    function GetSocket(Index: integer): TSock;
+    procedure FreeSocket(Index: integer);
+    constructor Create(Owner : TObject);
+    destructor Destroy; override;
   end;
 
 var
@@ -259,6 +293,147 @@ destructor THTTPClient.Destroy;
 begin
   HTTPSend.Free;
   PostVariables.Free;
+  inherited Destroy;
+end;
+
+{ TSocks }
+
+function TSocks.CreateSocket: integer;
+begin;
+  Result := SockList.Add(TSock.Create(Client));
+end;
+
+function TSocks.CreateSocketEx(Socket: TTCPBlockSocket): integer;
+begin;
+  Result := SockList.Add(TSock.Create(Client, Socket));
+end;
+
+function TSocks.GetSocket(Index: integer): TSock;
+begin
+  if (not (InRange(Index, 0, SockList.Count))) then
+    raise exception.CreateFmt('GetSocket: Trying to acces an index(%d) that is out of range', [index]);
+  if (SockList[index] = nil) then
+    raise exception.CreateFmt('GetSocket: Trying to acces an index(%d) that is freed', [index]);
+  Result := TSock(SockList[Index]);
+end;
+
+procedure TSocks.FreeSocket(Index: Integer);
+begin
+  if (not (InRange(Index, 0, SockList.Count))) then
+    raise exception.CreateFmt('GetSocket: Trying to free an index(%d) that is out of range', [index]);
+  if (SockList[index] = nil) then
+    raise exception.CreateFmt('GetSocket: Trying to free an index(%d) that is already freed', [index]);
+  TSock(SockList[Index]).Free;
+  SockList[Index] := nil;
+end;
+
+constructor TSocks.Create(Owner : TObject);
+begin
+  inherited Create;
+  Client := Owner;
+  SockList := TList.Create;
+end;
+
+destructor TSocks.Destroy;
+var
+  i: integer;
+begin
+  for i := SockList.Count - 1 downto 0 do
+    if SockList[i] <> nil then
+    begin
+      TSock(SockList[i]).Free;
+      TClient(Client).WriteLn(Format('Socket[%d] has not been freed in the script, freeing it now.',[i]));
+    end;
+  SockList.Free;
+  inherited Destroy;
+end;
+
+{ TSock }
+
+function TSock.RecvBufferStr(Length: integer): string;
+begin
+  Result := Sock.RecvBufferStr(Length, Timeout);
+  if (Sock.LastError <> 0) then
+     raise Exception.Create('Socket Error ' + IntToStr(Sock.LastError) + ': ' + Sock.LastErrorDesc);
+end;
+
+function TSock.RecvString: string;
+begin
+  Result := Sock.RecvString(Timeout);
+  if (Sock.LastError <> 0) then
+     raise Exception.Create('Socket Error ' + IntToStr(Sock.LastError) + ': ' + Sock.LastErrorDesc);
+end;
+
+function TSock.Recv: string;
+begin
+  Result := Sock.RecvPacket(Timeout);
+  if (Sock.LastError <> 0) then
+     raise Exception.Create('Socket Error ' + IntToStr(Sock.LastError) + ': ' + Sock.LastErrorDesc);
+end;
+
+procedure TSock.Send(Data: string);
+begin
+  Sock.SendString(Data);
+  if (Sock.LastError <> 0) then
+     raise Exception.Create('Socket Error ' + IntToStr(Sock.LastError) + ': ' + Sock.LastErrorDesc);
+end;
+
+procedure TSock.Connect(IP, Port: string);
+begin
+  Sock.Connect(IP, Port);
+  if (Sock.LastError <> 0) then
+     raise Exception.Create('Socket Error ' + IntToStr(Sock.LastError) + ': ' + Sock.LastErrorDesc);
+end;
+
+procedure TSock.Close;
+begin
+  Sock.CloseSocket;
+  if (Sock.LastError <> 0) then
+     raise Exception.Create('Socket Error ' + IntToStr(Sock.LastError) + ': ' + Sock.LastErrorDesc);
+end;
+
+procedure TSock.SetTimeout(Time: integer);
+begin
+  Timeout := Time;
+end;
+
+procedure TSock.Bind(IP, Port: string);
+begin
+  Sock.Bind(IP, Port);
+  if (Sock.LastError <> 0) then
+     raise Exception.Create('Socket Error ' + IntToStr(Sock.LastError) + ': ' + Sock.LastErrorDesc);
+end;
+
+procedure TSock.Listen;
+begin
+  Sock.Listen;
+end;
+
+function TSock.Accept: TTCPBlockSocket;
+var
+  Socket: TTCPBlockSocket;
+begin
+  Socket := TTCPBlockSocket.Create;
+  Socket.Socket := Sock.Accept;
+  Result := Socket;
+  if (Sock.LastError <> 0) then
+     raise Exception.Create('Socket Error ' + IntToStr(Sock.LastError) + ': ' + Sock.LastErrorDesc);
+end;
+
+constructor TSock.Create(Owner: TObject; Socket: TTCPBlockSocket = nil);
+begin
+  inherited Create;
+  Client := Owner;
+  Timeout := 1500;
+  if (Socket <> nil) then
+    Sock := Socket
+  else
+    Sock := TTCPBlockSocket.Create;
+end;
+
+destructor TSock.Destroy;
+begin
+  Sock.Free;
   inherited Destroy;
 end;
 
