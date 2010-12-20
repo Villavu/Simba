0aba4171c5cadfe57b5e26b0f3b360e48e6fcd8a
John P (Dgby714) <Dgby714@aol.com>
2010-12-20 02:00:24 -0500

    Revert "Simba: CurrThread.Socks"
    This reverts commit d4720a8b08dae4f8dcc64260b0741fef65aba7af.

--- Units/MMLAddon/mmlpsthread.pas
+++ Units/MMLAddon/mmlpsthread.pas
@@ -119,7 +119,6 @@ type
       Prop: TScriptProperties;
       Client : TClient;
       MInternet : TMInternet;
-      Socks: TSocks;
       StartTime : LongWord;
       Settings: TMMLSettings;
       SimbaSettingsFile: String;
@@ -310,7 +309,6 @@ begin
   if Assigned(OpenFileEvent) then
     Client.MFiles.OpenFileEvent := OpenFileEvent;
   MInternet := TMInternet.Create(Client);
-  Socks := TSocks.Create(Client);
   if Assigned(OpenConnectionEvent) then
     MInternet.OpenConnectionEvent := Self.OpenConnectionEvent;
   SyncInfo:= TheSyncInfo;
