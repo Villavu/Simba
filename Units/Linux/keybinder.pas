1b22a9737ec89e4919b5b6921dbf1bd253941852
Merlijn Wajer <merlijn@wizzup.org>
2011-04-09 20:45:58 +0200

    Simba: Keybinder: bind -> unbind.

--- Units/Linux/keybinder.pas
+++ Units/Linux/keybinder.pas
@@ -23,7 +23,7 @@ type TKeyBinderHandler = procedure (keystring: PChar; user_data: PtrUInt); cdecl
 procedure keybinder_init; cdecl; external libKeybinder;
 
 function keybinder_bind (keystring: PChar; handler: TKeyBinderHandler; user_data: PtrUInt): gboolean; cdecl; external libKeybinder;
-procedure keybinder_bind (keystring: PChar; handler: TKeyBinderHandler; user_data: PtrUInt); cdecl; external libKeybinder;
+procedure keybinder_unbind (keystring: PChar; handler: TKeyBinderHandler; user_data: PtrUInt); cdecl; external libKeybinder;
 
 function keybinder_get_current_event_time(): guint32;  cdecl; external libKeybinder;
 
