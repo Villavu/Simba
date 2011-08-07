4be1b4009d42ee215db3d5c108653c7249dfdab8
Merlijn Wajer <merlijn@wizzup.org>
2011-08-07 21:28:21 +0200

    Revert "Should compile with latest LCL, donno if it works, but meh, just graphic components."
    This reverts commit 981ec3760a5fdde59fcde6038e968b9cbd43a354.

--- Units/PascalScript/uPSI_ComCtrls.pas
+++ Units/PascalScript/uPSI_ComCtrls.pas
@@ -311,7 +311,7 @@ begin T := Self.TabIndex; end;
 
 (*----------------------------------------------------------------------------*)
 procedure TTabControlNoteBookStringsNoteBook_R(Self: TTabControlNoteBookStrings; var T: TNoteBook);
-begin T := TNoteBook(Self.NoteBook); end;
+begin T := Self.NoteBook; end;
 
 (*----------------------------------------------------------------------------*)
 procedure TTabControlStringsTabWidth_W(Self: TTabControlStrings; const T: Smallint);
