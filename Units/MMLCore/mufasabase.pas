b95b1015c21998bffcb1a4bb540c784cccb78122
Merlijn Wajer <merlijn@wizzup.org>
2011-11-30 21:12:28 +0100

    Bump version to 975.

--- Units/MMLCore/mufasabase.pas
+++ Units/MMLCore/mufasabase.pas
@@ -33,7 +33,7 @@ uses
 
 
 const
-    SimbaVersion = 974;
+    SimbaVersion = 975;
     SimbaMajor = 970; // this should be 980 even if SimbaVersion is 981, etc
 
 procedure mDebugLn( s : string);overload;
