f173d21face734f6765558c80db448358f6c7a05
Merlijn Wajer <merlijn@wizzup.org>
2011-12-07 19:38:52 +0100

    778.

--- Units/MMLCore/mufasabase.pas
+++ Units/MMLCore/mufasabase.pas
@@ -33,7 +33,7 @@ uses
 
 
 const
-    SimbaVersion = 977;
+    SimbaVersion = 978;
     SimbaMajor = 970; // this should be 980 even if SimbaVersion is 981, etc
 
 procedure mDebugLn( s : string);overload;
