4f4083706287e50d44c57d9d4cd5fdd5c8b27c95
Merlijn Wajer <merlijn@wizzup.org>
2011-12-24 17:38:41 +0100

    SimbaVersion -> 981.
    Former-commit-id: 32006424113421ae1c074fa06380d517ddcac266

--- Units/MMLCore/mufasabase.pas
+++ Units/MMLCore/mufasabase.pas
@@ -33,7 +33,7 @@ uses
 
 
 const
-    SimbaVersion = 980;
+    SimbaVersion = 981;
     SimbaMajor = 980; // this should be 980 even if SimbaVersion is 981, etc
 
 procedure mDebugLn( s : string);overload;
