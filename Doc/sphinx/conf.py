49c56a031d95dfb5f217a18413dea4be42f14a15
Merlijn Wajer <merlijn@wizzup.org>
2012-02-05 22:47:35 +0100

    Update copyright.
    Former-commit-id: 5aa5a6d8b137d33bfbd823fd741525c177bcc34f

--- Doc/sphinx/conf.py
+++ Doc/sphinx/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Simba'
-copyright = u'2011, Merlijn Wajer, Raymond van Venetie'
+copyright = u'2009-2012, Merlijn Wajer, Raymond van Venetie'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
