8ceef16bf02568e7487d125ede18cd35bf8b3233
John Peel <john@dgby.org>
2013-07-28 18:07:30 -0400

    Simba: Fix header in simbaunit.pas file. (Accedently modified copywrite)

--- Projects/Simba/simbaunit.pas
+++ Projects/Simba/simbaunit.pas
@@ -2,7 +2,7 @@
 	This file is part of the Mufasa Macro Library (MML)
 	Copyright (c) 2009-2012 by Raymond van Venetië and Merlijn Wajer
 
-    MML is free software: you can redistribute it and/or modify                                                    3
+    MML is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
