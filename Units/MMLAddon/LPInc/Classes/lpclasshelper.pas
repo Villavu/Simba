7aec8bd3873d3907f0a78487b62b67d5093ea996
John Peel <john@dgby.org>
2014-05-16 16:31:51 -0500

    Made a generic for Register Wrappers, and added TListBox.OnDrawItem
    Former-commit-id: 21ef4cc8b88ce3f3dc66bbcae6d851928680da80

--- Units/MMLAddon/LPInc/Classes/lpclasshelper.pas
+++ Units/MMLAddon/LPInc/Classes/lpclasshelper.pas
@@ -6,7 +6,7 @@ unit lpClassHelper;
 interface
 
 uses
-  Classes, SysUtils, lpcompiler, lptypes, lpvartypes, Controls;
+  Classes, SysUtils, lpcompiler, lptypes, lpvartypes, Controls, StdCtrls;
 
 type
   TLapeCompilerHelper = class helper for TLapeCompiler
@@ -15,15 +15,25 @@ type
     procedure addClassVar(const Obj, Item, Typ: string; const Read: Pointer; const Write: Pointer = nil; const Arr: boolean = False; const ArrType: string = 'UInt32');
   end;
 
+  generic TRegisterWrapper<_T> = class(TComponent)
+  private
+    FMethod: _T;
+  public
+    property InternalMethod: _T read FMethod write FMethod;
+  end;
+
+  PDrawItemEventWrapper = ^TDrawItemEventWrapper;
+  TDrawItemEventWrapper = procedure(Control: TControl; Index: Integer; ARect: TRect; State: TOwnerDrawState); cdecl;
+  TOnDrawItemWrapper = class(specialize TRegisterWrapper<TDrawItemEventWrapper>)
+  public
+    procedure DrawItem(Control: TWinControl; Index: Integer; ARect: TRect; State: TOwnerDrawState); register;
+  end;
+
   PMouseMoveEventWrapper = ^TMouseMoveEventWrapper;
   TMouseMoveEventWrapper = procedure(Sender: TObject; Shift: TShiftState; X, Y: Integer); cdecl;
-
-  TOnMouseMoveWrapper = class(TComponent)
-  private
-    FMouseMove: TMouseMoveEventWrapper;
+  TOnMouseMoveWrapper = class(specialize TRegisterWrapper<TMouseMoveEventWrapper>)
   public
     procedure MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer); register;
-    property InternalMouseMove: TMouseMoveEventWrapper read FMouseMove write FMouseMove;
   end;
 
 implementation
@@ -51,10 +61,16 @@ begin
     addGlobalFunc(Format('procedure %s.set%s(%sconst Value: %s);', [Obj, Item, Param, Typ]), Write);
 end;
 
+procedure TOnDrawItemWrapper.DrawItem(Control: TWinControl; Index: Integer; ARect: TRect; State: TOwnerDrawState); register;
+begin
+  if (Assigned(FMethod)) then
+    FMethod(TControl(Control), Index, ARect, State);
+end;
+
 procedure TOnMouseMoveWrapper.MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer); register;
 begin
-  if (Assigned(FMouseMove)) then
-    FMouseMove(Sender, Shift, X, Y);
+  if (Assigned(FMethod)) then
+    FMethod(Sender, Shift, X, Y);
 end;
 
 end.
