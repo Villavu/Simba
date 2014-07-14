2e6e7e01b6769aa0b75bfcbf2dbc6124d7f8de89
Jarl Holta <Jarlholta@gmail.com>
2014-07-14 13:27:04 +0200

    [Plugin] Option to export lape-style wrappers
    Added the option to use lape-style export wrapper-functions to plugins.
    By avoiding using the FFI, functions ...
    - avoids leaking the result, if the result is an array.
    - has around 5 times less call overhead
    
    To export lape-style wrapper functions an ekstra kw have to be added at
    the end of each function-string: `external;`.
    
    EG:
    >> procedure SomeFunc(const Params: PParamArray; const Result: Pointer);
    cdecl; export;
    ...
    >> ProcAddr := @SomeFunc;
    >> StrPCopy(ProcDef, 'function SomeFunc(Size:Int32): TIntegerArray;
    external;');
    That is how we know that we should not use the FFI.

--- Units/MMLAddon/mmlpsthread.pas
+++ Units/MMLAddon/mmlpsthread.pas
@@ -1540,7 +1540,7 @@ end;
 
 procedure TLPThread.LoadPlugin(plugidx: integer);
 var
-  I: integer;
+  I,len: integer;
   Wrapper: TImportClosure;
 begin
   with PluginsGlob.MPlugins[plugidx] do
@@ -1567,9 +1567,17 @@ begin
 
     for i := 0 to MethodLen - 1 do
     begin
-      Wrapper := LapeImportWrapper(Methods[i].FuncPtr, Compiler, Methods[i].FuncStr);
-      Compiler.addGlobalFunc(Methods[i].FuncStr, Wrapper.func);
-      ImportWrappers.Add(Wrapper);
+      len := length(Methods[i].FuncStr);
+      case LowerCase(Copy(Methods[i].FuncStr, len-8, len)) = 'external;' of
+        False:
+        begin
+          Wrapper := LapeImportWrapper(Methods[i].FuncPtr, Compiler, Methods[i].FuncStr);
+          Compiler.addGlobalFunc(Methods[i].FuncStr, Wrapper.func);
+          ImportWrappers.Add(Wrapper);
+        end;
+        True: //`external;` indicates that it's properly exported and using lapes wrapper-format.
+          Compiler.addGlobalFunc(Copy(Methods[i].FuncStr, 0, len-9), Methods[i].FuncPtr);
+      end;
     end;
 
     Compiler.EndImporting;
