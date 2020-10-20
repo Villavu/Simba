b221f24894d2f60031c8879e612b7cac735109c0
Olly <olly.cain@outlook.com>
2020-10-20 18:34:46 +0100

    Fix autobuild test

--- Source/Simba/macos/make-bundle.sh
+++ Source/Simba/macos/make-bundle.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 rm -r macos/Simba.app/Contents/MacOS/*
-cp ../../../Simba-Darwin64 macos/Simba.app/Contents/MacOS/
-cp -R macos/Simba.app ../../../Simba.app
-zip -r ../../../Simba-Darwin64.zip ../../../Simba.app
+cp ../../Simba-Darwin64 macos/Simba.app/Contents/MacOS/
+cp -R macos/Simba.app ../../Simba.app
+zip -r ../../Simba-Darwin64.zip ../../Simba.app
