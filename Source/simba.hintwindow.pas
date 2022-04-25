383e15458b8f61db5f2bc1c7de9b24d09468606e
Olly <olly.cain@outlook.com>
2022-04-25 19:12:29 +0100

    Improve TSimhaHintWindow rendering

--- Source/simba.hintwindow.pas
+++ Source/simba.hintwindow.pas
@@ -2,6 +2,8 @@
   Author: Raymond van Venetië and Merlijn Wajer
   Project: Simba (https://github.com/MerlijnWajer/Simba)
   License: GNU General Public License (https://www.gnu.org/licenses/gpl-3.0)
+
+  Hint window for TTreeview which ensures the hint is hidden under under multiple circumstances.
 }
 unit simba.hintwindow;
 
@@ -10,78 +12,107 @@ unit simba.hintwindow;
 interface
 
 uses
-  Classes, SysUtils, Controls, StdCtrls, Graphics, Forms;
+  Classes, SysUtils, Controls, ComCtrls, Graphics, Forms;
 
 type
-  TSimbaHintWindow = class(THintWindowRendered)
+  TSimbaHintWindow = class(TComponent)
   protected
-    FLabel: TLabel;
+    FTreeView: TTreeView;
+    FHintWindow: THintWindow;
 
-    procedure Paint; override;
-
-    procedure HandleNeedHide(Sender: TObject);
+    procedure DoHide(Sender: TObject);
   public
-    procedure ActivateHint(ARect: TRect; const AHint: String); override;  //Rename
-
-    constructor Create(AOwner: TComponent); override;
+    constructor Create(AOwner: TTreeView); reintroduce;
     destructor Destroy; override;
+
+    procedure Show(Node: TTreeNode; Caption: String);
+    procedure Hide;
   end;
 
 implementation
 
-procedure TSimbaHintWindow.Paint;
+type
+  TCustomHintWindow = class(THintWindow)
+  protected
+    procedure Paint; override;
+  end;
+
+procedure TCustomHintWindow.Paint;
+var
+  TextStyle: TTextStyle;
 begin
+  TextStyle := Default(TTextStyle);
+  TextStyle.Layout := tlCenter;
+
   Canvas.Pen.Color := clBlack;
   Canvas.Brush.Color := clWindow;
   Canvas.Rectangle(ClientRect);
 
-  inherited Paint();
+  Canvas.Font.Color := clWindowText;
+  Canvas.TextRect(ClientRect, 3, 0, Caption, TextStyle);
 end;
 
-// Dont use activate rendered
-procedure TSimbaHintWindow.ActivateHint(ARect: TRect; const AHint: String);
+procedure TSimbaHintWindow.DoHide(Sender: TObject);
 begin
-  if Visible and (FLabel.Caption = AHint) then
-    Exit;
-  FLabel.Caption := AHint;
-
-  ActivateWithBounds(ARect, AHint);
+  if (FHintWindow <> nil) then
+    FHintWindow.Hide()
 end;
 
-procedure TSimbaHintWindow.HandleNeedHide(Sender: TObject);
-begin
-  Visible := False;
-end;
-
-constructor TSimbaHintWindow.Create(AOwner: TComponent);
+constructor TSimbaHintWindow.Create(AOwner: TTreeView);
 begin
   inherited Create(AOwner);
 
-  if (AOwner is TControl) then
-    TControl(AOwner).AddHandlerOnVisibleChanged(@HandleNeedHide);
+  if (not (Owner is TTreeView)) then
+    raise Exception.Create('TSimbaHintWindow.Create: Owner is not a TTreeView');
 
-  AutoSize := True;
+  FTreeView := AOwner;
 
-  Font.Color := clWindowText;
+  FHintWindow := TCustomHintWindow.Create(Self);
+  FHintWindow.Font.Color := clWindowText;
 
-  FLabel := TLabel.Create(Self);
-  FLabel.BorderSpacing.Top := 2;
-  FLabel.BorderSpacing.Left := 5;
-  FLabel.BorderSpacing.Right := 5;
-  FLabel.BorderSpacing.Bottom := 2;
-  FLabel.Parent := Self;
-  FLabel.OptimalFill := True;
-  FLabel.AutoSize := True;
-  FLabel.Transparent := True;
+  FTreeView.OnMouseLeave := @DoHide; // Could do with a handler
+  FTreeView.AddHandlerOnVisibleChanged(@DoHide);
 
-  Application.AddOnDeactivateHandler(@HandleNeedHide);
+  Application.AddOnDeactivateHandler(@DoHide);
 end;
 
 destructor TSimbaHintWindow.Destroy;
 begin
-  Application.RemoveOnDeactivateHandler(@HandleNeedHide);
+  if (FHintWindow <> nil) then
+    FreeAndNil(FHintWindow);
+
+  if (FTreeView <> nil) then
+  begin
+    FTreeView.OnMouseLeave := nil;
+    FTreeView.RemoveHandlerOnVisibleChanged(@DoHide);
+  end;
+
+  if (Application <> nil) then
+    Application.RemoveOnDeactivateHandler(@DoHide);
 
   inherited Destroy();
+end;
+
+procedure TSimbaHintWindow.Show(Node: TTreeNode; Caption: String);
+var
+  NodeRect: TRect;
+begin
+  NodeRect.TopLeft := Node.TreeView.ClientToScreen(Node.DisplayRect(True).TopLeft);
+  NodeRect.BottomRight := Node.TreeView.ClientToScreen(Node.DisplayRect(True).BottomRight);
+
+  FHintWindow.HintRect := TRect.Create(
+    NodeRect.Left - 1,
+    NodeRect.Top - 2,
+    NodeRect.Left + FHintWindow.Canvas.TextWidth(Caption) + 6,
+    NodeRect.Bottom + 1
+  );
+
+  FHintWindow.ActivateHint(Caption);
+end;
+
+procedure TSimbaHintWindow.Hide;
+begin
+  FHintWindow.Visible := False;
 end;
 
 end.
