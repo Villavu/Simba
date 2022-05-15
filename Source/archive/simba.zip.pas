8c2f176a5b74237bcbbe458bebc0ddcc0439af03
ollydev <olly.cain@outlook.com>
2022-05-15 17:20:13 +0100

    Improve package extraction

--- Source/archive/simba.zip.pas
+++ Source/archive/simba.zip.pas
@@ -11,24 +11,18 @@ interface
 
 uses
   classes, sysutils, zipper,
-  simba.archive;
+  simba.archive, simba.files;
 
 type
   TSimbaZipExtractor = class(TSimbaArchiveExtractor)
   protected
-    FRoot: String;
-
-    procedure DoProgress(Sender: TObject; const Position, Size: Int64);
     procedure DoCreateInputStream(Sender: TObject; var Stream: TStream);
-    procedure DoCreateFileStream(Sender: TObject; var Stream: TStream; Item: TFullZipFileEntry);
+    procedure DoProgress(Sender: TObject; const Position, Size: Int64);
   public
     procedure Extract; override;
   end;
 
 implementation
-
-uses
-  LazFileUtils;
 
 procedure TSimbaZipExtractor.DoProgress(Sender: TObject; const Position, Size: Int64);
 begin
@@ -36,76 +30,31 @@ begin
     FOnProgress(Self, '', Position, Size);
 end;
 
-procedure TSimbaZipExtractor.DoCreateInputStream(Sender: TObject; var Stream: TStream);
-begin
-  Stream := TMemoryStream.Create();
-  Stream.CopyFrom(FInputStream, 0);
-end;
-
-procedure TSimbaZipExtractor.DoCreateFileStream(Sender: TObject; var Stream: TStream; Item: TFullZipFileEntry);
-var
-  I: Int32;
-  ArchiveFileName, DiskFileName: String;
-begin
-  if (Item.Index = 0) then
-    with Sender as TUnZipper do
-    begin
-      FRoot := Entries[0].ArchiveFileName;
-
-      for I := 0 to Entries.Count - 1 do
-        if not Entries[I].ArchiveFileName.StartsWith(FRoot) then
-          FRoot := '';
-    end;
-
-  ArchiveFileName := Copy(Item.ArchiveFileName, Length(FRoot) + 1);
-  if Flat then
-    DiskFileName := ExpandFileName(OutputPath + ExtractFileName(ArchiveFileName))
-  else
-    DiskFileName := ExpandFileName(OutputPath + ArchiveFileName);
-
-  if (ArchiveFileName = FRoot) then
-  begin
-    Stream := TMemoryStream.Create();
-
-    Exit;
-  end;
-
-  for I := 0 to FIgnoreList.Count - 1 do
-    if (FIgnoreList[I] = ArchiveFileName) or FileIsInPath(ArchiveFileName, FIgnoreList[I]) then
-    begin
-      Stream := TMemoryStream.Create();
-
-      Exit;
-    end;
-
-  if Item.IsDirectory then
-  begin
-    ForceDirectories(DiskFileName);
-
-    Exit;
-  end;
-
-  Stream := TFileStream.Create(DiskFileName, fmCreate);
-end;
-
 procedure TSimbaZipExtractor.Extract;
 var
   UnZipper: TUnZipper;
 begin
-  inherited Extract();
-
   UnZipper := TUnZipper.Create();
-  UnZipper.OutputPath := FOutputPath;
+  UnZipper.OutputPath := GetTempFileName(GetDataPath(), ExtractFileName(ExcludeTrailingPathDelimiter(FOutputPath)));
   UnZipper.OnOpenInputStream := @DoCreateInputStream;
-  UnZipper.OnCreateStream := @DoCreateFileStream;
   UnZipper.OnProgressEx := @DoProgress;
   UnZipper.Flat := FFlat;
 
   try
     UnZipper.UnZipAllFiles();
+
+    CopyToOutputPath(UnZipper.OutputPath);
   finally
     UnZipper.Free();
   end;
+end;
+
+procedure TSimbaZipExtractor.DoCreateInputStream(Sender: TObject; var Stream: TStream);
+begin
+  FInputStream.Position := 0;
+
+  Stream := TMemoryStream.Create();
+  Stream.CopyFrom(FInputStream, 0);
 end;
 
 end.
