6511b42099f54c3fa1908ff6de282c55fc6c874b
ollydev <olly.cain@outlook.com>
2022-09-03 03:55:31 +0100

    Docs

--- Source/script/imports/simba/simba.import_variant.pas
+++ Source/script/imports/simba/simba.import_variant.pas
@@ -17,12 +17,31 @@ uses
   classes, sysutils, lptypes,  variants,
   simba.script_compiler;
 
+
+(*
+Variant
+=======
+The variant "magic" datatype can store most base types.
+*)
+
 type
   {$SCOPEDENUMS ON}
   PVariantType = ^EVariantType;
   EVariantType = (Unknown, Unassigned, Null, Int8, Int16, Int32, Int64, UInt8, UInt16, UInt32, UInt64, Single, Double, DateTime, Currency, Boolean, Variant, AString, UString, WString);
   {$SCOPEDENUMS OFF}
 
+(*
+Variant.VarType
+~~~~~~~~~~~~~~~
+function Variant.VarType: EVariantVarType;
+
+Returns the variants var type.
+
+Example::
+
+  if (v.VarType = EVariantVarType.Int32) then
+    WriteLn('Variant contains a Int32');
+*)
 procedure _LapeVariantVarType(const Params: PParamArray; const Result: Pointer); {$IFDEF Lape_CDECL}cdecl;{$ENDIF}
 
   function GetVarType(const v: Variant): EVariantType;
@@ -57,36 +76,84 @@ begin
   PVariantType(Result)^ := GetVarType(PVariant(Params^[0])^);
 end;
 
+(*
+Variant.IsNumeric
+~~~~~~~~~~~~~~~~~
+function Variant.IsNumeric: Boolean;
+*)
 procedure _LapeVariantIsNumeric(const Params: PParamArray; const Result: Pointer); {$IFDEF Lape_CDECL}cdecl;{$ENDIF}
 begin
   PEvalBool(Result)^ := VarIsNumeric(PVariant(Params^[0])^);
 end;
 
+(*
+Variant.IsString
+~~~~~~~~~~~~~~~~
+function Variant.IsString: Boolean;
+*)
 procedure _LapeVariantIsString(const Params: PParamArray; const Result: Pointer); {$IFDEF Lape_CDECL}cdecl;{$ENDIF}
 begin
   PEvalBool(Result)^ := VarIsStr(PVariant(Params^[0])^);
 end;
 
+(*
+Variant.IsOrdinal
+~~~~~~~~~~~~~~~~~
+function Variant.IsOrdinal: Boolean;
+*)
 procedure _LapeVariantIsOrdinal(const Params: PParamArray; const Result: Pointer); {$IFDEF Lape_CDECL}cdecl;{$ENDIF}
 begin
   PEvalBool(Result)^ := VarIsOrdinal(PVariant(Params^[0])^);
 end;
 
+(*
+Variant.IsFloat
+~~~~~~~~~~~~~~~
+function Variant.IsFloat: Boolean;
+*)
 procedure _LapeVariantIsFloat(const Params: PParamArray; const Result: Pointer); {$IFDEF Lape_CDECL}cdecl;{$ENDIF}
 begin
   PEvalBool(Result)^ := VarIsFloat(PVariant(Params^[0])^);
 end;
 
+(*
+Variant.IsBoolean
+~~~~~~~~~~~~~~~~~
+function Variant.IsBoolean: Boolean;
+*)
 procedure _LapeVariantIsBoolean(const Params: PParamArray; const Result: Pointer); {$IFDEF Lape_CDECL}cdecl;{$ENDIF}
 begin
   PEvalBool(Result)^ := VarIsBool(PVariant(Params^[0])^);
 end;
 
+(*
+Variant.Null
+~~~~~~~~~~~~
+function Variant.Null: Variant; static;
+
+Static method that returns a null variant variable.
+
+Example::
+
+  v := Variant.Null;
+*)
 procedure _LapeVariantNull(const Params: PParamArray; const Result: Pointer); {$IFDEF Lape_CDECL}cdecl;{$ENDIF}
 begin
   PVariant(Result)^ := Null;
 end;
 
+(*
+Variant.Unassigned
+~~~~~~~~~~~~~~~~~~
+function Variant.Unassigned: Variant; static;
+
+Static method that returns a unassigned variant variable.
+
+Example::
+
+  if (v = Variant.Unassigned) then
+    WriteLn('The variant has not been assigned to!');
+*)
 procedure _LapeVariantUnassigned(const Params: PParamArray; const Result: Pointer); {$IFDEF Lape_CDECL}cdecl;{$ENDIF}
 begin
   PVariant(Result)^ := Unassigned;
