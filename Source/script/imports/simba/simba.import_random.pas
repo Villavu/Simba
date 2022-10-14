6780254c485e9c2447a380aa3a4f393b525b0dd4
ollydev <olly.cain@outlook.com>
2022-10-14 04:00:46 +0100

    More random methods
    Former-commit-id: 64f388fb1a84fcd6ac42012ea84b34a6b0660592

--- Source/script/imports/simba/simba.import_random.pas
+++ Source/script/imports/simba/simba.import_random.pas
@@ -1,3 +1,17 @@
+{
+  Author: Raymond van Venetië and Merlijn Wajer
+  Project: Simba (https://github.com/MerlijnWajer/Simba)
+  License: GNU General Public License (https://www.gnu.org/licenses/gpl-3.0)
+}
+
+{
+ Jarl Holta - https://github.com/slackydev
+
+  - RandomLeft
+  - RandomRight
+  - RandomMean
+  - RandomMode
+}
 unit simba.import_random;
 
 {$i simba.inc}
@@ -19,16 +33,97 @@ Methods relating to generating random numbers.
 (*
 RandomCenterTPA
 ~~~~~~~~~~~~~~~
-function RandomCenterTPA(Amount:Integer; Box: TBox): TPointArray;
+function RandomCenterTPA(Amount: Integer; Box: TBox): TPointArray;
+
+Generates random points in `Box` weighted towards the center.
 *)
 procedure _LapeRandomCenterTPA(const Params: PParamArray; const Result: Pointer); {$IFDEF Lape_CDECL}cdecl;{$ENDIF}
 begin
   PPointArray(Result)^ := RandomCenterTPA(PInteger(Params^[0])^, PBox(Params^[1])^);
 end;
 
+(*
+RandomTPA
+~~~~~~~~~
+function RandomTPA(Amount: Integer; Box: TBox): TPointArray;
+
+Generates random points in `Box`.
+*)
 procedure _LapeRandomTPA(const Params: PParamArray; const Result: Pointer); {$IFDEF Lape_CDECL}cdecl;{$ENDIF}
 begin
   PPointArray(Result)^ := RandomTPA(PInteger(Params^[0])^, PBox(Params^[1])^);
+end;
+
+(*
+RandomLeft
+~~~~~~~~~~
+function RandomLeft(Lo, Hi: Double): Double;
+function RandomLeft(Lo, Hi: Int64): Int64;
+
+Generates a random number between `Lo` and `Hi` weighted towards `Lo`
+*)
+procedure _LapeRandomLeft(const Params: PParamArray; const Result: Pointer); {$IFDEF Lape_CDECL}cdecl;{$ENDIF}
+begin
+  PDouble(Result)^ := RandomLeft(PDouble(Params^[0])^, PDouble(Params^[1])^);
+end;
+
+procedure _LapeRandomLeftI(const Params: PParamArray; const Result: Pointer); {$IFDEF Lape_CDECL}cdecl;{$ENDIF}
+begin
+  PInt64(Result)^ := RandomLeft(PInt64(Params^[0])^, PInt64(Params^[1])^);
+end;
+
+(*
+RandomRight
+~~~~~~~~~~~
+function RandomRight(Lo, Hi: Double): Double;
+function RandomRight(Lo, Hi: Int64): Int64;
+
+Generates a random number between `Lo` and `Hi` weighted towards `Hi`
+*)
+procedure _LapeRandomRight(const Params: PParamArray; const Result: Pointer); {$IFDEF Lape_CDECL}cdecl;{$ENDIF}
+begin
+  PDouble(Result)^ := RandomRight(PDouble(Params^[0])^, PDouble(Params^[1])^);
+end;
+
+procedure _LapeRandomRightI(const Params: PParamArray; const Result: Pointer); {$IFDEF Lape_CDECL}cdecl;{$ENDIF}
+begin
+  PInt64(Result)^ := RandomRight(PInt64(Params^[0])^, PInt64(Params^[1])^);
+end;
+
+(*
+RandomMean
+~~~~~~~~~~
+function RandomMean(Lo, Hi: Double): Double;
+function RandomMean(Lo, Hi: Int64): Int64;
+
+Generates a random number between `Lo` and `Hi` weighted towards the mean of Lo,Hi
+*)
+procedure _LapeRandomMean(const Params: PParamArray; const Result: Pointer); {$IFDEF Lape_CDECL}cdecl;{$ENDIF}
+begin
+  PDouble(Result)^ := RandomMean(PDouble(Params^[0])^, PDouble(Params^[1])^);
+end;
+
+procedure _LapeRandomMeanI(const Params: PParamArray; const Result: Pointer); {$IFDEF Lape_CDECL}cdecl;{$ENDIF}
+begin
+  PInt64(Result)^ := RandomMean(PInt64(Params^[0])^, PInt64(Params^[1])^);
+end;
+
+(*
+RandomMode
+~~~~~~~~~~
+function RandomMode(Mode, Lo, Hi: Double): Double;
+function RandomMode(Mode, Lo, Hi: Int64): Int64;
+
+Generates a random number between `Lo` and `Hi` weighted torwards `Mode`
+*)
+procedure _LapeRandomMode(const Params: PParamArray; const Result: Pointer); {$IFDEF Lape_CDECL}cdecl;{$ENDIF}
+begin
+  PDouble(Result)^ := RandomMode(PDouble(Params^[0])^, PDouble(Params^[1])^, PDouble(Params^[2])^);
+end;
+
+procedure _LapeRandomModeI(const Params: PParamArray; const Result: Pointer); {$IFDEF Lape_CDECL}cdecl;{$ENDIF}
+begin
+  PInt64(Result)^ := RandomMode(PInt64(Params^[0])^, PInt64(Params^[1])^, PInt64(Params^[2])^);
 end;
 
 procedure ImportRandom(Compiler: TSimbaScript_Compiler);
@@ -37,8 +132,22 @@ begin
   begin
     ImportingSection := 'Random';
 
+    addGlobalVar(ltDouble, @RandCutoff, 'RandCutoff');
+
     addGlobalFunc('function RandomCenterTPA(Amount: Integer; Box: TBox): TPointArray', @_LapeRandomCenterTPA);
     addGlobalFunc('function RandomTPA(Amount: Integer; Box: TBox): TPointArray', @_LapeRandomTPA);
+
+    addGlobalFunc('function RandomLeft(Lo, Hi: Double): Double; overload', @_LapeRandomLeft);
+    addGlobalFunc('function RandomLeft(Lo, Hi: Int64): Int64; overload', @_LapeRandomLeftI);
+
+    addGlobalFunc('function RandomRight(Lo, Hi: Double): Double; overload', @_LapeRandomRight);
+    addGlobalFunc('function RandomRight(Lo, Hi: Int64): Int64; overload', @_LapeRandomRightI);
+
+    addGlobalFunc('function RandomMean(Lo, Hi: Double): Double; overload', @_LapeRandomMean);
+    addGlobalFunc('function RandomMean(Lo, Hi: Int64): Int64; overload', @_LapeRandomMeanI);
+
+    addGlobalFunc('function RandomMode(Mode, Lo, Hi: Double): Double; overload', @_LapeRandomMode);
+    addGlobalFunc('function RandomMode(Mode, Lo, Hi: Int64): Int64; overload', @_LapeRandomModeI);
 
     ImportingSection := '';
   end;
@@ -48,3 +157,4 @@ initialization
   TSimbaScript_Compiler.RegisterImport(@ImportRandom);
 
 end.
+
