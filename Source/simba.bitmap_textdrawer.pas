83c73b113160af258746c0fb6475d5742c792ecc
Olly <Olly.cain@outlook.com>
2023-05-26 01:35:38 +0100

    Text drawing improvements: italic, bold, loading fonts and cache.
    Former-commit-id: f208de5aab453b4cb2a8f8fdd9aedf3fc0aeb8d2

--- Source/simba.bitmap_textdrawer.pas
+++ Source/simba.bitmap_textdrawer.pas
@@ -10,14 +10,36 @@ unit simba.bitmap_textdrawer;
 interface
 
 uses
-  classes, sysutils, lazfreetypefpimagedrawer, easylazfreetype, fpimage, graphics,
-  simba.mufasatypes, simba.colormath;
+  Classes, SysUtils, Graphics, FPImage, LazFreeTypeFPImageDrawer, EasyLazFreeType,
+  simba.mufasatypes, simba.simplelock;
 
 type
+  TSimbaFreeTypeFontLoader = class
+  protected
+  type
+    TFontCacheEntry = record
+      Font: TFreeTypeFont;
+      Name: String;
+    end;
+    TFontCache = array of TFontCacheEntry;
+  protected
+    FFonts: TFontCache;
+    FFontNames: TStringArray;
+    FLock: TSimpleEnterableLock;
+    FSystemFontsLoaded: Boolean;
+
+    procedure LoadSystemFonts;
+  public
+    function LoadFonts(Dir: String): Boolean;
+    function GetFont(AName: String; ASize: Single; AAntialised, ABold, AItalic: Boolean): TFreeTypeFont;
+
+    property FontNames: TStringArray read FFontNames;
+  end;
+
   TSimbaTextDrawer = class(TFPImageFreeTypeDrawer)
   protected
-    FWidth: Int32;
-    FHeight: Int32;
+    FWidth: Integer;
+    FHeight: Integer;
     FData: PColorBGRA;
     FCurrentColor: PColorBGRA;
     FBitmap: TObject;
@@ -25,86 +47,89 @@ type
     FFont: TFreeTypeFont;
     FFontName: String;
     FFontAntialised: Boolean;
-    FFontSize: Single;
+    FSize: Single;
     FClipRect: TRect;
+    FBold: Boolean;
+    FItalic: Boolean;
+    FLock: TSimpleEnterableLock;
 
     procedure MoveToPixel(X, Y: Integer); override;
     function GetCurrentColor: TFPColor; override;
     procedure SetCurrentColorAndMoveRight(const AColor: TFPColor); override;
     procedure MoveRight; override;
     function GetClipRect: TRect; override;
-    function GetFonts: TStringArray;
-    procedure PrepareDrawing;
+    procedure BeginDrawing;
+    procedure EndDrawing;
   public
-    property Fonts: TStringArray read GetFonts;
     property FontName: String read FFontName write FFontName;
-    property FontSize: Single read FFontSize write FFontSize;
+    property Size: Single read FSize write FSize;
     property Antialiased: Boolean read FFontAntialised write FFontAntialised;
+    property Bold: Boolean read FBold write FBold;
+    property Italic: Boolean read FItalic write FItalic;
 
     procedure DrawText(Text: String; Position: TPoint; Color: TColor); overload;
     procedure DrawText(Text: String; Box: TBox; Center: Boolean; Color: TColor); overload;
 
-    function TextWidth(Text: String): Int32;
-    function TextHeight(Text: String): Int32;
+    function TextWidth(Text: String): Integer;
+    function TextHeight(Text: String): Integer;
     function TextSize(Text: String): TPoint;
 
     constructor Create(Bitmap: TObject); reintroduce;
-    destructor Destroy; override;
   end;
+
+var
+  SimbaFreeTypeFontLoader: TSimbaFreeTypeFontLoader = nil;
 
 implementation
 
 uses
-  fileutil, lazfileutils, syncobjs, lazfreetypefontcollection,
+  Forms, FileUtil, LazFileUtils, LazFreeTypeFontCollection,
   simba.bitmap;
 
+procedure TSimbaFreeTypeFontLoader.LoadSystemFonts;
 var
-  FontsLoaded: Boolean = False;
-  FontNames: TStringArray;
-  FontsCriticalSection: TCriticalSection;
-
-procedure LoadFonts;
-var
-  SearchPaths, FontsFiles: TStringList;
-  I: Int32;
+  SearchPaths, FontFiles: TStringList;
+  I: Integer;
 begin
-  FontsCriticalSection.Enter();
+  if FSystemFontsLoaded then
+    Exit;
+  FSystemFontsLoaded := True;
+
+  SearchPaths := nil;
+  FontFiles := nil;
 
   try
-    if FontsLoaded then
-      Exit;
-    FontsLoaded := True;
-
     SearchPaths := TStringList.Create();
+    SearchPaths.Add(Application.Location);
 
     {$IFDEF WINDOWS}
     SearchPaths.Add(SHGetFolderPathUTF8(20));
     {$ENDIF}
+
     {$IFDEF LINUX}
-    SearchPaths.Add('/usr/share/cups/fonts/');
-    SearchPaths.Add('/usr/share/fonts/truetype/');
-    SearchPaths.Add('/usr/local/lib/X11/fonts/');
+    SearchPaths.Add('/usr/share/fonts/');
+    SearchPaths.Add('/usr/local/share/fonts');
     SearchPaths.Add(GetUserDir() + '.fonts/');
+    SearchPaths.Add(GetUserDir() + '.local/share/fonts/');
     {$ENDIF}
+
     {$IFDEF DARWIN}
     SearchPaths.Add('/Library/Fonts/');
     SearchPaths.Add('/System/Library/Fonts/');
-    SearchPaths.Add('/Network/Library/Fonts/');
-    SearchPaths.Add('~/Library/Fonts/');
     {$ENDIF}
 
-    FontsFiles := TStringList.Create();
-    FontsFiles.Sorted := True;
+    FontFiles := TStringList.Create();
+    FontFiles.Duplicates := dupIgnore;
+    FontFiles.Sorted := True;
 
     for I := 0 to SearchPaths.Count - 1 do
-      FindAllFiles(FontsFiles, ExpandFileName(SearchPaths[I]), '*.ttf', True);
+      FindAllFiles(FontFiles, ExpandFileName(SearchPaths[I]), '*.ttf');
 
     FontCollection.BeginUpdate();
-
     try
-      for I := 0 to FontsFiles.Count - 1 do
+      for I := 0 to FontFiles.Count - 1 do
       try
-        FontCollection.AddFile(FontsFiles[I]);
+        FontCollection.AddFile(FontFiles[I]);
       except
         // ignore exceptions due to font read errors
       end;
@@ -112,41 +137,122 @@ begin
       FontCollection.EndUpdate();
     end;
 
+    FFontNames := [];
     with FontCollection.FamilyEnumerator do
-      while MoveNext do
-        FontNames += [Current.FamilyName];
-
-    SearchPaths.Free();
-    FontsFiles.Free();
+      while MoveNext() do
+        FFontNames += [Current.FamilyName];
   finally
-    FontsCriticalSection.Leave();
+    if Assigned(SearchPaths) then
+      SearchPaths.Free();
+    if Assigned(FontFiles) then
+      FontFiles.Free();
   end;
 end;
 
-function LoadFont(Name: String; AStyle: TFreeTypeStyles = []): TFreeTypeFont;
+function TSimbaFreeTypeFontLoader.LoadFonts(Dir: String): Boolean;
 var
+  FontFiles: TStringList;
+  I: Integer;
+begin
+  Result := False;
+
+  FLock.Enter();
+  try
+    FontFiles := FindAllFiles(ExpandFileName(Dir), '*.ttf', False);
+
+    FontCollection.BeginUpdate();
+    try
+      for I := 0 to FontFiles.Count - 1 do
+      try
+        FontCollection.AddFile(FontFiles[I]);
+
+        Result := True;
+      except
+        // ignore exceptions due to font read errors
+      end;
+    finally
+      FontCollection.EndUpdate();
+    end;
+
+    FFontNames := [];
+    with FontCollection.FamilyEnumerator do
+      while MoveNext() do
+        FFontNames += [Current.FamilyName];
+  finally
+    if Assigned(FontFiles) then
+      FontFiles.Free();
+
+    FLock.Leave();
+  end;
+end;
+
+function TSimbaFreeTypeFontLoader.GetFont(AName: String; ASize: Single; AAntialised, ABold, AItalic: Boolean): TFreeTypeFont;
+var
+  I: Integer;
+  Entry: TFontCacheEntry;
+  Style: TFreeTypeStyles;
+  StyleAsStr: String;
+  Quality: TGlyphRenderQuality;
   FamilyItem: TCustomFamilyCollectionItem;
   FontItem: TCustomFontCollectionItem;
-  Style: String = '';
 begin
   Result := nil;
 
-  LoadFonts();
+  FLock.Enter();
+  try
+    LoadSystemFonts();
 
-  FamilyItem := FontCollection.Family[Name];
-  if (FamilyItem <> nil) then
-  begin
-    if (ftsBold in AStyle) then
-      Style := 'Bold';
-    if (ftsItalic in AStyle) then
-      Style := Style + ' Italic';
+    if AAntialised then
+      Quality := grqHighQuality
+    else
+      Quality := grqMonochrome;
 
-    FontItem := FamilyItem.GetFont(Style);
-    if (FontItem <> nil) then
+    Style := [];
+    if ABold   then Style += [ftsBold];
+    if AItalic then Style += [ftsItalic];
+
+    for I := 0 to High(FFonts) do
     begin
-      Result := FontItem.CreateFont;
-      Result.Style := AStyle;
+      Entry := FFonts[I];
+
+      if (Result = nil) and
+         (Entry.Name = AName) and
+         (Entry.Font.SizeInPixels = ASize) and
+         (Entry.Font.Style = Style) and
+         (Entry.Font.ClearType = AAntialised) and
+         (Entry.Font.Quality = Quality) then
+        begin
+          Result := Entry.Font;
+          Exit;
+        end;
     end;
+
+    StyleAsStr := '';
+    if ABold   then StyleAsStr += ' Bold';
+    if AItalic then StyleAsStr += ' Italic';
+
+    FamilyItem := FontCollection.Family[AName];
+    if Assigned(FamilyItem) then
+    begin
+      FontItem := FamilyItem.GetFont(StyleAsStr);
+
+      if Assigned(FontItem) then
+      begin
+        Result := FontItem.CreateFont();
+
+        Entry.Name := AName;
+        Entry.Font := Result;
+        Entry.Font.Style := Style;
+        Entry.Font.SizeInPixels := ASize;
+        Entry.Font.KerningEnabled := False;
+        Entry.Font.Quality := Quality;
+        Entry.Font.ClearType := AAntialised;
+
+        FFonts += [Entry];
+      end;
+    end;
+  finally
+    FLock.Leave();
   end;
 end;
 
@@ -183,15 +289,10 @@ begin
   Result := FClipRect;
 end;
 
-function TSimbaTextDrawer.GetFonts: TStringArray;
+procedure TSimbaTextDrawer.BeginDrawing;
 begin
-  PrepareDrawing();
+  FLock.Enter();
 
-  Result := FontNames;
-end;
-
-procedure TSimbaTextDrawer.PrepareDrawing;
-begin
   with TMufasaBitmap(FBitmap) do
   begin
     Self.FData := Data;
@@ -202,102 +303,87 @@ begin
   FClipRect.Right := FWidth;
   FClipRect.Bottom := FHeight;
 
-  if (FFont <> nil) and (FFontName <> FFont.Family) then
-  begin
-    FreeAndNil(FFont);
-
-    FFont := LoadFont(FFontName);
-    if (FFont <> nil) then
-      FFontName := FFont.Family;
-  end;
-
+  FFont := SimbaFreeTypeFontLoader.GetFont(FFontName, FSize, FFontAntialised, FBold, FItalic);
   if (FFont = nil) then
-  begin
-    FFont := LoadFont(FFontName);
-    if (FFont = nil) then
-      raise Exception.Create('No fonts found');
+    SimbaException('Font "%s" not found', [FFontName]);
+end;
 
-    FFontName := FFont.Family;
-  end;
-
-  case FFontAntialised of
-    True:
-      begin
-        FFont.Quality := grqHighQuality;
-        FFont.ClearType := True;
-      end;
-
-    False:
-      begin
-        FFont.Quality := grqMonochrome;
-        FFont.ClearType := False;
-      end;
-  end;
-
-  FFont.SizeInPixels := FFontSize;
+procedure TSimbaTextDrawer.EndDrawing;
+begin
+  FLock.Leave();
 end;
 
 constructor TSimbaTextDrawer.Create(Bitmap: TObject);
 begin
   FBitmap := Bitmap;
-
-  FFontSize := 18;
+  FSize := 18;
   FFontAntialised := True;
-end;
-
-destructor TSimbaTextDrawer.Destroy;
-begin
-  if (FFont <> nil) then
-    FreeAndNil(FFont);
-
-  inherited Destroy();
 end;
 
 procedure TSimbaTextDrawer.DrawText(Text: String; Position: TPoint; Color: TColor);
 begin
-  PrepareDrawing();
-
-  inherited DrawText(Text, FFont, Position.X, Position.Y + FFont.SizeInPoints, TColorToFPColor(Color));
+  BeginDrawing();
+  try
+    inherited DrawText(Text, FFont, Position.X, Position.Y + FFont.SizeInPoints, TColorToFPColor(Color));
+  finally
+    EndDrawing();
+  end;
 end;
 
 procedure TSimbaTextDrawer.DrawText(Text: String; Box: TBox; Center: Boolean; Color: TColor);
 begin
-  PrepareDrawing();
+  BeginDrawing();
 
-  if Center then
-    inherited DrawTextRect(Text, FFont, Box.X1, Box.Y1, Box.X2, Box.Y2, TColorToFPColor(Color), [ftaCenter, ftaVerticalCenter])
-  else
-    inherited DrawTextRect(Text, FFont, Box.X1, Box.Y1, Box.X2, Box.Y2, TColorToFPColor(Color), [ftaLeft, ftaTop]);
+  try
+    if Center then
+      inherited DrawTextRect(Text, FFont, Box.X1, Box.Y1, Box.X2, Box.Y2, TColorToFPColor(Color), [ftaCenter, ftaVerticalCenter])
+    else
+      inherited DrawTextRect(Text, FFont, Box.X1, Box.Y1, Box.X2, Box.Y2, TColorToFPColor(Color), [ftaLeft, ftaTop]);
+  finally
+    EndDrawing();
+  end;
 end;
 
-function TSimbaTextDrawer.TextWidth(Text: String): Int32;
+function TSimbaTextDrawer.TextWidth(Text: String): Integer;
 begin
-  PrepareDrawing();
+  BeginDrawing();
 
-  Result := Round(FFont.TextWidth(Text));
+  try
+    Result := Round(FFont.TextWidth(Text));
+  finally
+    EndDrawing();
+  end;
 end;
 
-function TSimbaTextDrawer.TextHeight(Text: String): Int32;
+function TSimbaTextDrawer.TextHeight(Text: String): Integer;
 begin
-  PrepareDrawing();
+  BeginDrawing();
 
-  Result := Round(FFont.TextHeight(Text));
+  try
+    Result := Round(FFont.TextHeight(Text));
+  finally
+    EndDrawing();
+  end;
 end;
 
 function TSimbaTextDrawer.TextSize(Text: String): TPoint;
 begin
-  PrepareDrawing();
+  BeginDrawing();
 
-  Result.X := Round(FFont.TextWidth(Text));
-  Result.Y := Round(FFont.TextHeight(Text));
+  try
+    Result.X := Round(FFont.TextWidth(Text));
+    Result.Y := Round(FFont.TextHeight(Text));
+  finally
+    EndDrawing();
+  end;
 end;
 
 initialization
-  FontsCriticalSection := TCriticalSection.Create();
+  SimbaFreeTypeFontLoader := TSimbaFreeTypeFontLoader.Create();
 
 finalization
-  if (FontsCriticalSection <> nil) then
-    FontsCriticalSection.Free();
+  if Assigned(SimbaFreeTypeFontLoader) then
+    FreeAndNil(SimbaFreeTypeFontLoader);
 
 end.
 
