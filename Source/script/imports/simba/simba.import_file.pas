33ef3fda4f65de721a660d023aabb3790859b85d
Olly <Olly.cain@outlook.com>
2023-07-10 01:52:08 +0100

    Finish new file interface

--- Source/script/imports/simba/simba.import_file.pas
+++ Source/script/imports/simba/simba.import_file.pas
@@ -33,19 +33,24 @@ begin
   DeleteINI(PString(Params^[0])^, PString(Params^[1])^, PString(Params^[1])^);
 end;
 
-procedure _LapeUnZipFile(const Params: PParamArray); LAPE_WRAPPER_CALLING_CONV
+procedure _LapeZipExtractAll(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
-  UnZipFile(PString(Params^[0])^, PString(Params^[1])^);
+  PBoolean(Result)^ := ZipExtractAll(PString(Params^[0])^, PString(Params^[1])^);
 end;
 
-procedure _LapeUnZipOneFile(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
+procedure _LapeZipExtractOne(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
-  PBoolean(Result)^ := UnZipOneFile(PString(Params^[0])^, PString(Params^[1])^, PString(Params^[2])^);
+  PBoolean(Result)^ := ZipExtractOne(PString(Params^[0])^, PString(Params^[1])^, PString(Params^[2])^);
 end;
 
-procedure _LapeZipFiles(const Params: PParamArray); LAPE_WRAPPER_CALLING_CONV
+procedure _LapeZipFiles(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
-  ZipFiles(PString(Params^[0])^, PStringArray(Params^[1])^);
+  PBoolean(Result)^ := ZipFiles(PString(Params^[0])^, PStringArray(Params^[1])^);
+end;
+
+procedure _LapeZipEntries(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
+begin
+  PStringArray(Result)^ := ZipEntries(PString(Params^[0])^);
 end;
 
 procedure _LapeFileAppend(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
@@ -213,6 +218,11 @@ begin
   PString(Result)^ := TSimbaPath.PathIncludeLeadingSep(PString(Params^[0])^);
 end;
 
+procedure _LapePathExtractRelative(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
+begin
+  PString(Result)^ := TSimbaPath.PathExtractRelative(PString(Params^[0])^, PString(Params^[1])^);
+end;
+
 procedure _LapeDirList(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PStringArray(Result)^ := TSimbaDir.DirList(PString(Params^[0])^, PBoolean(Params^[1])^);
@@ -273,38 +283,14 @@ begin
   PString(Result)^ := GetTempFileName();
 end;
 
-procedure _LapeGetEnvVar(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
-begin
-  PString(Result)^ := GetEnvironmentVariable(PString(Params^[0])^);
-end;
-
-procedure _LapeGetEnvVars(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
-
-  function _GetEnvVars: TStringArray;
-  var
-    Count, I: Integer;
-  begin
-    Count := 0;
-
-    SetLength(Result, GetEnvironmentVariableCount() + 1);
-    for I := 1 to GetEnvironmentVariableCount() do
-      if (GetEnvironmentString(I) <> '') then
-      begin
-        Result[Count] := GetEnvironmentString(I);
-        Inc(Count);
-      end;
-    SetLength(Result, Count);
-  end;
-
-begin
-  PStringArray(Result)^ := _GetEnvVars();
-end;
-
 procedure ImportFile(Compiler: TSimbaScript_Compiler);
 begin
   with Compiler do
   begin
     ImportingSection := 'File';
+
+    addGlobalVar(PATH_SEP, 'PATH_SEP').isConstant := True;
+    addGlobalVar(LINE_SEP, 'LINE_SEP').isConstant := True;
 
     addGlobalVar(GetIncludePath(), 'IncludePath').isConstant := True;
     addGlobalVar(GetPluginPath(), 'PluginPath').isConstant := True;
@@ -316,16 +302,15 @@ begin
     addGlobalFunc('procedure WriteINI(Section, KeyName, NewString, FileName: String)', @_LapeWriteINI);
     addGlobalFunc('function ReadINI(Section, KeyName, FileName: String): String', @_LapeReadINI);
     addGlobalFunc('procedure DeleteINI(Section, KeyName, FileName: String)', @_LapeDeleteINI);
-    addGlobalFunc('procedure ZipFiles(ArchiveFileName: String; const Files: TStringArray)', @_LapeZipFiles);
-    addGlobalFunc('procedure UnZipFile(ArchiveFileName, OutputDirectory: String)', @_LapeUnZipFile);
-    addGlobalFunc('function UnZipOneFile(ArchiveFileName, FileName, OutputDirectory: String): Boolean', @_LapeUnZipOneFile);
+
+    addGlobalFunc('function ZipExtractAll(ZipFileName, OutputDir: String): Boolean', @_LapeZipExtractAll);
+    addGlobalFunc('function ZipExtractOne(ZipFileName, FileName, OutputDir: String): Boolean', @_LapeZipExtractOne);
+    addGlobalFunc('function ZipFiles(ZipFileName: String; Files: TStringArray): Boolean', @_LapeZipFiles);
+    addGlobalFunc('function ZipEntries(ZipFileName: String): TStringArray', @_LapeZipEntries);
 
     addGlobalFunc('function GetUserDir: String', @_LapeGetUserDir);
     addGlobalFunc('function GetTempDir: String', @_LapeGetTempDir);
     addGlobalFunc('function GetTempFileName: String', @_LapeGetTempFileName);
-
-    addGlobalFunc('function GetEnvVar(Name: String): String', @_LapeGetEnvVar);
-    addGlobalFunc('function GetEnvVars: TStringArray', @_LapeGetEnvVars);
 
     addGlobalFunc('function FileRead(FileName: String): String', @_LapeFileRead);
     addGlobalFunc('function FileReadEx(FileName: String; Offset: Integer): String', @_LapeFileReadEx);
@@ -363,6 +348,7 @@ begin
     addGlobalFunc('function PathIncludeTrailingSep(Path: String): String', @_LapePathIncludeTrailingSep);
     addGlobalFunc('function PathExcludeLeadingSep(Path: String): String', @_LapePathExcludeLeadingSep);
     addGlobalFunc('function PathIncludeLeadingSep(Path: String): String', @_LapePathIncludeLeadingSep);
+    addGlobalFunc('function PathExtractRelative(BasePath, DestPath: String): String', @_LapePathExtractRelative);
 
     addGlobalFunc('function DirList(Path: String; Recursive: Boolean = False): TStringArray', @_LapeDirList);
     addGlobalFunc('function DirSearch(Path: String; Mask: String; Recursive: Boolean = False): TStringArray', @_LapeDirSearch);
