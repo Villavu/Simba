b96a560dbe824843e1e88c0aaf8e030ff464b857
Olly <Olly.cain@outlook.com>
2023-08-16 23:58:29 +0100

    More doc headers
    Former-commit-id: aabd51a53b00db61f3d96b1725b3fc9b47b53472

--- Source/script/imports/simba/simba.import_matchtemplate.pas
+++ Source/script/imports/simba/simba.import_matchtemplate.pas
@@ -21,46 +21,97 @@ uses
   lptypes,
   simba.bitmap, simba.matchtemplate;
 
+(*
+Match Template
+==============
+Template matching
+*)
+
+(*
+TMatchTemplateCache.Create
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+> function TMatchTemplateCache.Create(Image, Template: TIntegerMatrix; Formula: ETMFormula): TMatchTemplateCache; static;
+*)
 procedure _LapeMatchTemplateCache_Create(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PMatchTemplateCacheBase(Result)^ := MatchTemplateCache(PIntegerMatrix(Params^[0])^, PIntegerMatrix(Params^[1])^, PTMFormula(Params^[2])^);
 end;
 
+(*
+TMatchTemplateCache.Create
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+> function TMatchTemplateCache.Create(Image, Template: TImage; Formula: ETMFormula): TMatchTemplateCache; static;
+*)
 procedure _LapeMatchTemplateCache_CreateEx(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PMatchTemplateCacheBase(Result)^ := MatchTemplateCache(PSimbaImage(Params^[0])^.ToMatrixBGR(), PSimbaImage(Params^[1])^.ToMatrixBGR(), PTMFormula(Params^[2])^);
 end;
 
+(*
+TMatchTemplateCache.FreeOnTerminate
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+> procedure TMatchTemplateCache.FreeOnTerminate(Enable: Boolean);
+*)
 procedure _LapeMatchTemplateCache_FreeOnTerminate(const Params: PParamArray); LAPE_WRAPPER_CALLING_CONV
 begin
   PMatchTemplateCacheBase(Params^[0])^.FreeOnTerminate := PBoolean(Params^[1])^;
 end;
 
+(*
+MatchTemplateMask
+~~~~~~~~~~~~~~~~~
+> function MatchTemplateMask(Cache: TMatchTemplateCache; Template: TIntegerMatrix; Formula: ETMFormula): TSingleMatrix;
+*)
 procedure _LapeMatchTemplateMaskCache(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PSingleMatrix(Result)^ := MatchTemplateMask(PMatchTemplateCacheBase(Params^[0])^, PIntegerMatrix(Params^[1])^, PTMFormula(Params^[2])^);
 end;
 
+(*
+MatchTemplateMask
+~~~~~~~~~~~~~~~~~
+> function MatchTemplateMask(Cache: TMatchTemplateCache; Template: TImage; Formula: ETMFormula): TSingleMatrix;
+*)
 procedure _LapeMatchTemplateMaskCacheEx(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PSingleMatrix(Result)^ := MatchTemplateMask(PMatchTemplateCacheBase(Params^[0])^, PSimbaImage(Params^[1])^.ToMatrixBGR(), PTMFormula(Params^[2])^);
 end;
 
+(*
+MatchTemplateMask
+~~~~~~~~~~~~~~~~~
+> function MatchTemplateMask(Image, Template: TIntegerMatrix; Formula: ETMFormula): TSingleMatrix;
+*)
 procedure _LapeMatchTemplateMask(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PSingleMatrix(Result)^ := MatchTemplateMask(PIntegerMatrix(Params^[0])^, PIntegerMatrix(Params^[1])^, PTMFormula(Params^[2])^);
 end;
 
+(*
+MatchTemplate
+~~~~~~~~~~~~~
+> function MatchTemplate(Image, Template: TIntegerMatrix; Formula: ETMFormula): TSingleMatrix;
+*)
 procedure _LapeMatchTemplate(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PSingleMatrix(Result)^ := MatchTemplate(PIntegerMatrix(Params^[0])^, PIntegerMatrix(Params^[1])^, PTMFormula(Params^[2])^);
 end;
 
+(*
+TImage.MatchTemplate
+~~~~~~~~~~~~~~~~~~~~
+> function TImage.MatchTemplate(Template: TImage; Formula: ETMFormula): TSingleMatrix;
+*)
 procedure _LapeMufasaBitmap_MatchTemplate(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PSingleMatrix(Result)^ := PSimbaImage(Params^[0])^.MatchTemplate(PSimbaImage(Params^[1])^, PTMFormula(Params^[2])^);
 end;
 
+(*
+TImage.MatchTemplateMask
+~~~~~~~~~~~~~~~~~~~~~~~~
+> function TImage.MatchTemplateMask(Template: TImage; Formula: ETMFormula): TSingleMatrix;
+*)
 procedure _LapeMufasaBitmap_MatchTemplateMask(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PSingleMatrix(Result)^ := PSimbaImage(Params^[0])^.MatchTemplateMask(PSimbaImage(Params^[1])^, PTMFormula(Params^[2])^);
@@ -95,4 +146,3 @@ begin
 end;
 
 end.
-
--- Source/script/imports/simba/simba.import_math.pas
+++ Source/script/imports/simba/simba.import_math.pas
@@ -16,161 +16,327 @@ uses
   lptypes, math,
   simba.math, simba.geometry;
 
+(*
+Math
+====
+Math functions
+*)
+
+(*
+Distance
+~~~~~~~~
+> function Distance(const X1, Y1, X2, Y2: Integer): Integer;
+*)
 procedure _LapeDistance(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PInteger(Result)^ := Distance(PInteger(Params^[0])^, PInteger(Params^[1])^, PInteger(Params^[2])^, PInteger(Params^[3])^);
 end;
 
+(*
+Distance
+~~~~~~~~
+> function Distance(const P1, P2: TPoint): Integer;
+*)
 procedure _LapeDistanceEx(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PInteger(Result)^ := Distance(PPoint(Params^[0])^, PPoint(Params^[1])^);
 end;
 
+(*
+FixD
+~~~~
+> function FixD(Deg: Double): Double;
+*)
 procedure _LapeFixD(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PDouble(Result)^ := FixD(PDouble(Params^[0])^);
 end;
 
+(*
+LogN
+~~~~
+> function LogN(base, x: Double): Double;
+*)
 procedure _LapeLogn(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PDouble(Result)^ := logn(PDouble(Params^[0])^, PDouble(Params^[1])^);
 end;
 
+(*
+Sar
+~~~
+> function Sar(x: Integer; Shift: Byte): Integer;
+*)
 procedure _LapeSar(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   Plongint(Result)^ := SarLongint(Plongint(Params^[0])^, PByte(Params^[1])^);
 end;
 
+(*
+Ror
+~~~
+> function Ror(x: UInt32; Shift: Byte): UInt32;
+*)
 procedure _LapeRor(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PLongWord(Result)^ := RorDWord(Plongword(Params^[0])^, PByte(Params^[1])^);
 end;
 
+(*
+Rol
+~~~
+> function Rol(x: UInt32; Shift: Byte): UInt32;
+*)
 procedure _LapeRol(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PLongWord(Result)^ := RolDWord(Plongword(Params^[0])^, PByte(Params^[1])^);
 end;
 
+(*
+Radians
+~~~~~~~
+> function Radians(Deg: Double): Double;
+*)
 procedure _LapeRadians(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PDouble(Result)^ := Radians(PDouble(Params^[0])^);
 end;
 
+(*
+Degrees
+~~~~~~~
+> function Degrees(Rad: Double): Double;
+*)
 procedure _LapeDegrees(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PDouble(Result)^ := Degrees(PDouble(Params^[0])^);
 end;
 
+(*
+Log2
+~~~~
+> function Log2(x: Double): Double;
+*)
 procedure _LapeLog2(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PDouble(Result)^ := Log2(PDouble(Params^[0])^);
 end;
 
+(*
+Log10
+~~~~~
+> function Log10(x: Double): Double;
+*)
 procedure _LapeLog10(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PDouble(Result)^ := Log10(PDouble(Params^[0])^);
 end;
 
+(*
+FixRad
+~~~~~~
+> function FixRad(Rad: Double): Double;
+*)
 procedure _LapeFixRad(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PDouble(Result)^ := FixRad(PDouble(Params^[0])^);
 end;
 
+(*
+NextPower2
+~~~~~~~~~~
+> function NextPower2(const n: Integer): Integer;
+*)
 procedure _LapeNextPower2(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PInteger(Result)^ := NextPower2(PInteger(Params^[0])^);
 end;
 
+(*
+Modulo
+~~~~~~
+> function Modulo(const X, Y: Integer): Integer;
+*)
 procedure _LapeModulo(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PInteger(Result)^ := Modulo(PInteger(Params^[0])^, PInteger(Params^[1])^);
 end;
 
+(*
+Modulo
+~~~~~~
+> function Modulo(const X, Y: Double): Double;
+*)
 procedure _LapeModuloF(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PDouble(Result)^ := Modulo(PDouble(Params^[0])^, PDouble(Params^[1])^);
 end;
 
+(*
+DeltaAngle
+~~~~~~~~~~
+> function DeltaAngle(const DegreesA, DegreesB: Double; R: Double = 360): Double;
+*)
 procedure _LapeDeltaAngle(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PDouble(Result)^ := TSimbaGeometry.DeltaAngle(PDouble(Params^[0])^, PDouble(Params^[1])^, PDouble(Params^[2])^);
 end;
 
+(*
+ExpandPolygon
+~~~~~~~~~~~~~
+> function ExpandPolygon(const Polygon: TPointArray; Amount: Integer): TPointArray;
+*)
 procedure _LapeExpandPolygon(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PPointArray(Result)^ := TSimbaGeometry.ExpandPolygon(PPointArray(Params^[0])^, PInteger(Params^[1])^);
 end;
 
+(*
+PolygonArea
+~~~~~~~~~~~
+> function PolygonArea(const Polygon: TPointArray): Double;
+*)
 procedure _LapePolygonArea(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PDouble(Result)^ := TSimbaGeometry.PolygonArea(PPointArray(Params^[0])^);
 end;
 
+(*
+CrossProduct
+~~~~~~~~~~~~
+> function CrossProduct(const r, p, q: TPoint): Int64;
+*)
 procedure _LapeCrossProduct1(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PInt64(Result)^ := TSimbaGeometry.CrossProduct(PPoint(Params^[0])^, PPoint(Params^[1])^, PPoint(Params^[2])^);
 end;
 
+(*
+CrossProduct
+~~~~~~~~~~~~
+> function CrossProduct(const rx,ry, px,py, qx,qy: Double): Double;
+*)
 procedure _LapeCrossProduct2(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PDouble(Result)^ := TSimbaGeometry.CrossProduct(PDouble(Params^[0])^, PDouble(Params^[1])^, PDouble(Params^[2])^, PDouble(Params^[3])^, PDouble(Params^[4])^, PDouble(Params^[5])^);
 end;
 
+(*
+LinesIntersect
+~~~~~~~~~~~~~~
+> function LinesIntersect(const P1, P2, Q1, Q2: TPoint): Boolean;
+*)
 procedure _LapeLinesIntersect1(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PBoolean(Result)^ := TSimbaGeometry.LinesIntersect(PPoint(Params^[0])^, PPoint(Params^[1])^, PPoint(Params^[2])^, PPoint(Params^[3])^);
 end;
 
+(*
+LinesIntersect
+~~~~~~~~~~~~~~
+> function LinesIntersect(const P1, P2, Q1, Q2: TPoint; out Where: TPoint): Boolean;
+*)
 procedure _LapeLinesIntersect2(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PBoolean(Result)^ := TSimbaGeometry.LinesIntersect(PPoint(Params^[0])^, PPoint(Params^[1])^, PPoint(Params^[2])^, PPoint(Params^[3])^, PPoint(Params^[4])^);
 end;
 
+(*
+DistToLine
+~~~~~~~~~~
+> function DistToLine(const P, P1, P2: TPoint; out Nearest: TPoint): Double;
+*)
 procedure _LapeDistToLine1(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PDouble(Result)^ := TSimbaGeometry.DistToLine(PPoint(Params^[0])^, PPoint(Params^[1])^, PPoint(Params^[2])^, PPoint(Params^[3])^);
 end;
 
+(*
+DistToLine
+~~~~~~~~~~
+> function DistToLine(const P, P1, P2: TPoint): Double;
+*)
 procedure _LapeDistToLine2(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PDouble(Result)^ := TSimbaGeometry.DistToLine(PPoint(Params^[0])^, PPoint(Params^[1])^, PPoint(Params^[2])^);
 end;
 
+(*
+PointInTriangle
+~~~~~~~~~~~~~~~
+> function PointInTriangle(const P, P1, P2, P3: TPoint): Boolean;
+*)
 procedure _LapePointInTriangle(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PBoolean(Result)^ := TSimbaGeometry.PointInTriangle(PPoint(Params^[0])^, PPoint(Params^[1])^, PPoint(Params^[2])^, PPoint(Params^[3])^);
 end;
 
+(*
+PointInBox
+~~~~~~~~~~
+> function PointInBox(const P: TPoint; const Box: TBox): Boolean;
+*)
 procedure _LapePointInBox(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PBoolean(Result)^ := TSimbaGeometry.PointInBox(PPoint(Params^[0])^, PBox(Params^[1])^);
 end;
 
+(*
+PointInQuad
+~~~~~~~~~~~
+> function PointInQuad(const P: TPoint; const A,B,C,D: TPoint): Boolean;
+*)
 procedure _LapePointInQuad(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PBoolean(Result)^ := TSimbaGeometry.PointInQuad(PPoint(Params^[0])^, PPoint(Params^[1])^, PPoint(Params^[2])^, PPoint(Params^[3])^, PPoint(Params^[4])^);
 end;
 
+(*
+PointInPolygon
+~~~~~~~~~~~~~~
+> function PointInPolygon(const P: TPoint; const Polygon: TPointArray): Boolean;
+*)
 procedure _LapePointInPolygon(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PBoolean(Result)^ := TSimbaGeometry.PointInPolygon(PPoint(Params^[0])^, PPointArray(Params^[1])^);
 end;
 
+(*
+PointInCircle
+~~~~~~~~~~~~~
+> function PointInCircle(const P, Center: TPoint; Radius: Double): Boolean; static;
+*)
 procedure _LapePointInCircle(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PBoolean(Result)^ := TSimbaGeometry.PointInCircle(PPoint(Params^[0])^, PPoint(Params^[1])^, PDouble(Params^[2])^);
 end;
 
+(*
+PointInEllipse
+~~~~~~~~~~~~~~
+> function PointInEllipse(const P, Center: TPoint; const YRadius, XRadius: Double): Boolean;
+*)
 procedure _LapePointInEllipse(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PBoolean(Result)^ := TSimbaGeometry.PointInEllipse(PPoint(Params^[0])^, PPoint(Params^[1])^, PDouble(Params^[2])^, PDouble(Params^[3])^);
 end;
 
+(*
+IsNumber
+~~~~~~~~
+> function IsNumber(const Value: Single): Boolean;
+*)
 procedure _LapeIsNumberS(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PBoolean(Result)^ := IsNumber(PSingle(Params^[0])^);
 end;
 
+(*
+IsNumber
+~~~~~~~~
+> function IsNumber(const Value: Double): Boolean;
+*)
 procedure _LapeIsNumberD(const Params: PParamArray; const Result: Pointer); LAPE_WRAPPER_CALLING_CONV
 begin
   PBoolean(Result)^ := IsNumber(PDouble(Params^[0])^);
