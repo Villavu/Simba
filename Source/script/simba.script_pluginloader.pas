16e32acdd0a449e0a0c1779af9ed24be5b141c2a
Olly <Olly.cain@outlook.com>
2023-09-26 16:50:47 +0100

    LoadPlugin: On Unix use RTLD_NOLOAD to check if library is already loaded

--- Source/script/simba.script_pluginloader.pas
+++ Source/script/simba.script_pluginloader.pas
@@ -12,12 +12,21 @@ function LoadPlugin(FileName: String; ExtraSearchDirs: TStringArray = nil): TLib
 implementation
 
 uses
-  simba.mufasatypes, simba.env;
+  simba.mufasatypes, simba.env
+  {$IFDEF UNIX},
+  dl
+  {$ENDIF};
 
 function LoadPlugin(FileName: String; ExtraSearchDirs: TStringArray): TLibHandle;
 begin
   if (not FindPlugin(FileName, ExtraSearchDirs)) then
     raise Exception.CreateFmt('Unable to find plugin "%s"', [FileName]);
+
+  {$IFDEF UNIX}
+  Result := TLibHandle(dlopen(PChar(FileName), RTLD_NOLOAD));
+  if (Result <> NilHandle) then
+    Exit;
+  {$ENDIF}
 
   Result := LoadLibrary(FileName);
 
