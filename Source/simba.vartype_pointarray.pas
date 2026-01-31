1b2fd1014681e7f8d2e1f306b3480bf27d5330a5
cww <craigmilde_@hotmail.com>
2026-01-31 15:00:34 -0500

    just both 4 neighbour

--- Source/simba.vartype_pointarray.pas
+++ Source/simba.vartype_pointarray.pas
@@ -1129,19 +1129,18 @@ begin
   B.Y2 := (B.Y2 - B.Y1) + Iterations + 1;
 
   Matrix.SetSize(B.X2, B.Y2);
-  SetLength(Enqueued, B.Y2, B.X2);
-
+  Enqueued.SetSize(B.X2, B.Y2);
   for I:=0 to High(Self) do
     Matrix[Self[I].Y - B.Y1][Self[I].X - B.X1] := True;
 
-  SetLength(face, 8);
+  SetLength(face, 4);
 
   Edges := Self.Edges().Offset(-B.X1, -B.Y1);
   QueueA.Init();
-  for i := 0 to High(Edges) do
+  for I:=0 to High(Edges) do
   begin
-    pt := Edges[i];
-    if Matrix[pt.y][pt.x] and (not Enqueued[pt.y][pt.x]) then
+    pt := Edges[I];
+    if Matrix[pt.y][pt.x] and not Enqueued[pt.y][pt.x] then
     begin
       Enqueued[pt.y][pt.x] := True;
       QueueA.Add(pt);
@@ -1157,11 +1156,11 @@ begin
         begin
           pt := QueueA.Pop;
           Matrix[pt.y][pt.x] := False;
-          GetAdjacent8(face, pt);
-          for I:=0 to 7 do
+          GetAdjacent4(face, pt);
+          for I:=0 to 3 do
           begin
             pt := face[I];
-            if Matrix[pt.y][pt.x] and (not Enqueued[pt.y][pt.x]) then
+            if Matrix[pt.y][pt.x] and not Enqueued[pt.y][pt.x] then
             begin
               Enqueued[pt.y][pt.x] := True;
               QueueB.Add(pt);
@@ -1173,8 +1172,8 @@ begin
         begin
           pt := QueueB.Pop;
           Matrix[pt.y][pt.x] := False;
-          GetAdjacent8(face, pt);
-          for I:=0 to 7 do
+          GetAdjacent4(face, pt);
+          for I:=0 to 3 do
           begin
             pt := face[I];
             if Matrix[pt.y][pt.x] and (not Enqueued[pt.y][pt.x]) then
@@ -1219,7 +1218,7 @@ begin
   for I:=0 to High(Self) do
     Matrix[Self[I].Y - B.Y1][Self[I].X - B.X1] := True;
 
-  SetLength(face,8);
+  SetLength(face,4);
   QueueA.InitWith(Self.Edges().Offset(-B.X1,-B.Y1));
   QueueB.Init();
   J := 0;
@@ -1228,8 +1227,8 @@ begin
     True:
       while (QueueA.Count > 0) do
       begin
-        GetAdjacent8(face, QueueA.Pop());
-        for I:=0 to 7 do
+        GetAdjacent4(face, QueueA.Pop());
+        for I:=0 to 3 do
         begin
           pt := face[I];
           if not(Matrix[pt.y][pt.x]) then
@@ -1243,8 +1242,8 @@ begin
     False:
       while (QueueB.Count > 0) do
       begin
-        GetAdjacent8(face, QueueB.Pop());
-        for I:=0 to 7 do
+        GetAdjacent4(face, QueueB.Pop());
+        for I:=0 to 3 do
         begin
           pt := face[I];
           if not(Matrix[pt.y][pt.x]) then
