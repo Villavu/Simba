c3e2459fc714bfdc364138ee575c799dd98e7bad
MilesWilde <tinkerworks@proton.me>
2026-03-09 22:33:31 -0400

    Make Linux build able to target on Wayland (#557)

--- Source/simba.nativeinterface_linux.pas
+++ Source/simba.nativeinterface_linux.pas
@@ -663,7 +663,14 @@ end;
 
 function TSimbaNativeInterface_Linux.GetWindowAtCursor(Exclude: TWindowHandleArray): TWindowHandle;
 
-  function ShouldReturnWindow(Window: TWindow): Boolean;
+  function NormalizeWindow(Window: TWindow): TWindow;
+  begin
+    Result := GetRootWindow(Window);
+    if (Result = 0) then
+      Result := Window;
+  end;
+
+  function IsUsableWindow(Window: TWindow): Boolean;
   var
     B: TBox;
   begin
@@ -675,31 +682,61 @@ function TSimbaNativeInterface_Linux.GetWindowAtCursor(Exclude: TWindowHandleArr
   end;
 
 var
-  Root, Child: TWindow;
+  Root, Child, Current: TWindow;
   x_root, y_root, x, y: Integer;
   mask: UInt32;
   I: Integer;
   Windows: array[UInt8] of TWindow;
   WindowCount: Integer;
+  RawCandidate, NormalizedCandidate: TWindow;
 begin
   Result := 0;
   WindowCount := 0;
 
-  SimbaXLib.XQueryPointer(GetDesktopWindow(), @Root, @Child, @x_root, @y_root, @x, @y, @mask);
-  while SimbaXLib.XQueryPointer(Child, @Root, @Child, @x_root, @y_root, @x, @y, @mask) do
+  if not SimbaXLib.XQueryPointer(GetDesktopWindow(), @Root, @Child, @x_root, @y_root, @x, @y, @mask) then
+  begin
+    Exit(0);
+  end;
+
+  if (Child = 0) then
+  begin
+    Exit(0);
+  end;
+
+  Current := Child;
+
+  // IMPORTANT: include the first child returned from the root query
+  Windows[WindowCount] := Current;
+  Inc(WindowCount);
+
+  while SimbaXLib.XQueryPointer(Current, @Root, @Child, @x_root, @y_root, @x, @y, @mask) do
   begin
     if (Child = 0) then
       Break;
-    Windows[WindowCount] := Child;
+
+    Current := Child;
+    Windows[WindowCount] := Current;
     Inc(WindowCount);
+
     if (WindowCount > High(Windows)) then
-      Exit;
+      Break;
   end;
 
-  // now go back until we find something worthwhile
   for I := WindowCount - 1 downto 0 do
-    if ShouldReturnWindow(Windows[I]) then
-      Exit(Windows[I])
+  begin
+    RawCandidate := Windows[I];
+
+    if IsUsableWindow(RawCandidate) then
+    begin
+      Exit(RawCandidate);
+    end;
+
+    NormalizedCandidate := NormalizeWindow(RawCandidate);
+    if IsUsableWindow(NormalizedCandidate) then
+    begin
+      Exit(NormalizedCandidate);
+    end;
+  end;
 end;
 
 function TSimbaNativeInterface_Linux.GetDesktopWindow: TWindowHandle;
@@ -725,8 +762,14 @@ begin
 end;
 
 function TSimbaNativeInterface_Linux.IsWindowVisible(Window: TWindowHandle): Boolean;
+var
+  Attr: TXWindowAttributes;
 begin
-  Result := HasWindowProperty(GetRootWindow(Window), SimbaXLib.WM_STATE);
+  if (Window = 0) then
+    Exit;
+
+  if SimbaXLib.XGetWindowAttributes(Window, @Attr) then
+    Result := Attr.map_state = IsViewable;
 end;
 
 function TSimbaNativeInterface_Linux.GetWindowPID(Window: TWindowHandle): Integer;
